57584de3d8589d0b703755d9e25b890fea3b4632
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-12 13:31:34 +0800

    Add solution: 2289.steps-to-make-array-non-decreasing.py

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -7,33 +7,31 @@
 # @lc code=start
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        # ans will store the maximum number of steps required
-        ans = 0
-        # stack will store pairs of (value, step_at_which_it_is_removed)
-        stack = []
+        n = len(nums)
+        # dp[i] will store the step at which nums[i] is removed.
+        # If dp[i] is 0, nums[i] is never removed.
+        dp = [0] * n
+        stack = [] # Monotonic stack to store indices of elements
         
-        for x in nums:
-            # t represents the time (step) when the current element x is removed
-            t = 0
-            # Pop elements from the stack that are smaller than or equal to x.
-            # x can only be removed after all these elements are removed.
-            while stack and x >= stack[-1][0]:
-                t = max(t, stack.pop()[1])
+        for i in range(n):
+            current_max_dp = 0
+            # Pop elements that are smaller than or equal to the current element.
+            # These elements would be removed before or at the same time as 
+            # the current element if they were to the right of a larger element.
+            while stack and nums[stack[-1]] <= nums[i]:
+                current_max_dp = max(current_max_dp, dp[stack.pop()])
             
-            # If the stack is empty, it means no element to the left is greater than x.
-            # Therefore, x will never be removed.
-            if not stack:
-                t = 0
+            # If the stack is not empty, the top element is the nearest element
+            # to the left that is strictly greater than nums[i].
+            if stack:
+                # nums[i] will be removed 1 step after all elements between 
+                # it and nums[stack[-1]] are removed.
+                dp[i] = current_max_dp + 1
             else:
-                # If the stack is not empty, the top of the stack is the nearest element
-                # to the left that is strictly greater than x. x will be removed
-                # one step after the maximum removal time of the elements between them.
-                t = t + 1
+                # No element to the left is greater than nums[i].
+                dp[i] = 0
             
-            # Update the global maximum steps
-            ans = max(ans, t)
-            # Push the current element and its removal time onto the stack
-            stack.append((x, t))
+            stack.append(i)
             
-        return ans
+        return max(dp)
 # @lc code=end
