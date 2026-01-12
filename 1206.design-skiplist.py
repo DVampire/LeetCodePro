f62bce778ae7419da520b73eefc13fba90f464ea
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-12 13:25:55 +0800

    Add solution: 1206.design-skiplist.py

--- 1206.design-skiplist.py
+++ 1206.design-skiplist.py
@@ -8,62 +8,66 @@ import random
 
 # @lc code=start
 class Node:
-    __slots__ = 'val', 'next'
-    def __init__(self, val, level):
+    __slots__ = 'val', 'right', 'down'
+    def __init__(self, val=-1, right=None, down=None):
         self.val = val
-        self.next = [None] * level
+        self.right = right
+        self.down = down
 
 class Skiplist:
-
     def __init__(self):
-        self.max_level = 16
-        self.head = Node(-1, self.max_level)
-
-    def _random_level(self) -> int:
-        lvl = 1
-        while lvl < self.max_level and random.random() < 0.5:
-            lvl += 1
-        return lvl
+        # Initialize with a dummy head node
+        self.head = Node()
 
     def search(self, target: int) -> bool:
         curr = self.head
-        for i in range(self.max_level - 1, -1, -1):
-            while curr.next[i] and curr.next[i].val < target:
-                curr = curr.next[i]
-            if curr.next[i] and curr.next[i].val == target:
+        while curr:
+            # Move right as far as possible
+            while curr.right and curr.right.val < target:
+                curr = curr.right
+            # Check if found
+            if curr.right and curr.right.val == target:
                 return True
+            # Move down to the next level
+            curr = curr.down
         return False
 
     def add(self, num: int) -> None:
-        update = [None] * self.max_level
+        # Track the path of predecessors
+        nodes = []
         curr = self.head
-        for i in range(self.max_level - 1, -1, -1):
-            while curr.next[i] and curr.next[i].val < num:
-                curr = curr.next[i]
-            update[i] = curr
+        while curr:
+            while curr.right and curr.right.val < num:
+                curr = curr.right
+            nodes.append(curr)
+            curr = curr.down
         
-        lvl = self._random_level()
-        node = Node(num, lvl)
-        for i in range(lvl):
-            node.next[i] = update[i].next[i]
-            update[i].next[i] = node
+        down_node = None
+        should_insert = True
+        # Insert from bottom up using the predecessors path
+        while should_insert and nodes:
+            prev = nodes.pop()
+            prev.right = Node(num, prev.right, down_node)
+            down_node = prev.right
+            # 50% chance to promote to a higher level
+            should_insert = random.random() < 0.5
+        
+        # If we still need to promote but ran out of levels, create a new level
+        if should_insert:
+            self.head = Node(-1, Node(num, None, down_node), self.head)
 
     def erase(self, num: int) -> bool:
-        update = [None] * self.max_level
         curr = self.head
-        for i in range(self.max_level - 1, -1, -1):
-            while curr.next[i] and curr.next[i].val < num:
-                curr = curr.next[i]
-            update[i] = curr
-        
-        node = update[0].next[0]
-        if not node or node.val != num:
-            return False
-        
-        for i in range(len(node.next)):
-            update[i].next[i] = node.next[i]
-        return True
-
+        found = False
+        while curr:
+            while curr.right and curr.right.val < num:
+                curr = curr.right
+            # If found at this level, remove it and continue to lower levels
+            if curr.right and curr.right.val == num:
+                found = True
+                curr.right = curr.right.right
+            curr = curr.down
+        return found
 
 # Your Skiplist object will be instantiated and called as such:
 # obj = Skiplist()
