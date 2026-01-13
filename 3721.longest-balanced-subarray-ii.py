e42628a3f13def5f089805f324946c4c5a850c22
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 03:02:01 +0800

    Add solution: 3721.longest-balanced-subarray-ii.py

--- 3721.longest-balanced-subarray-ii.py
+++ 3721.longest-balanced-subarray-ii.py
@@ -5,80 +5,117 @@
 #
 
 # @lc code=start
-import sys
+from typing import List
+
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)
+
+    def add(self, i: int, delta: int) -> None:
+        n = self.n
+        bit = self.bit
+        while i <= n:
+            bit[i] += delta
+            i += i & -i
+
+    def sum(self, i: int) -> int:
+        s = 0
+        bit = self.bit
+        while i > 0:
+            s += bit[i]
+            i -= i & -i
+        return s
+
+    def range_sum(self, l: int, r: int) -> int:
+        if l > r:
+            return 0
+        return self.sum(r) - self.sum(l - 1)
+
 
 class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 0:
-            return 0
-        
-        # Power of 2 for segment tree leaves
-        size = 1
-        while size < n:
-            size *= 2
-            
-        inf = n + 7
-        tree_min = [inf] * (2 * size)
-        tree_max = [inf] * (2 * size)
-        lazy = [0] * (2 * size)
-        
-        def push_up(node):
-            tree_min[node] = min(tree_min[2 * node], tree_min[2 * node + 1])
-            tree_max[node] = max(tree_max[2 * node], tree_max[2 * node + 1])
 
-        def apply(node, val):
-            tree_min[node] += val
-            tree_max[node] += val
-            lazy[node] += val
+        total = [0] * (4 * n)
+        minSuf = [0] * (4 * n)
+        maxSuf = [0] * (4 * n)
 
-        def push_down(node):
-            if lazy[node] != 0:
-                apply(2 * node, lazy[node])
-                apply(2 * node + 1, lazy[node])
-                lazy[node] = 0
+        def pull(idx: int) -> None:
+            li = idx * 2
+            ri = li + 1
+            total[idx] = total[li] + total[ri]
+            # suffix either starts in right, or starts in left (then add total of right)
+            minSuf[idx] = min(minSuf[ri], minSuf[li] + total[ri])
+            maxSuf[idx] = max(maxSuf[ri], maxSuf[li] + total[ri])
 
-        def update(node, node_l, node_r, l, r, val):
-            if l <= node_l and node_r <= r:
-                apply(node, val)
+        def update(idx: int, l: int, r: int, pos: int, val: int) -> None:
+            if l == r:
+                total[idx] = val
+                minSuf[idx] = val
+                maxSuf[idx] = val
                 return
-            push_down(node)
-            mid = (node_l + node_r) // 2
-            if l <= mid:
-                update(2 * node, node_l, mid, l, r, val)
-            if r > mid:
-                update(2 * node + 1, mid + 1, node_r, l, r, val)
-            push_up(node)
+            mid = (l + r) // 2
+            if pos <= mid:
+                update(idx * 2, l, mid, pos, val)
+            else:
+                update(idx * 2 + 1, mid + 1, r, pos, val)
+            pull(idx)
 
-        def find_leftmost(node, node_l, node_r):
-            if tree_min[node] > 0 or tree_max[node] < 0:
-                return -1
-            if node_l == node_r:
-                return node_l
-            push_down(node)
-            mid = (node_l + node_r) // 2
-            res = find_leftmost(2 * node, node_l, mid)
-            if res == -1:
-                res = find_leftmost(2 * node + 1, mid + 1, node_r)
-            return res
+        bit = Fenwick(n)
+        last = {}  # value -> last index
+        w_at = [0] * (n + 1)  # current w at position
+
+        # Find the smallest i in [1..qr] s.t. sum(w[i..qr]) == -sumRight
+        # Here we always call with sumRight=0 from the root.
+        def find_leftmost(idx: int, l: int, r: int, qr: int, sumRight: int):
+            if l > qr:
+                return None
+            if r <= qr:
+                target = -sumRight
+                if target < minSuf[idx] or target > maxSuf[idx]:
+                    return None
+                if l == r:
+                    return l
+                mid = (l + r) // 2
+                # full coverage => right child fully included
+                right_sum_included = total[idx * 2 + 1]
+                res = find_leftmost(idx * 2, l, mid, qr, sumRight + right_sum_included)
+                if res is not None:
+                    return res
+                return find_leftmost(idx * 2 + 1, mid + 1, r, qr, sumRight)
+            else:
+                mid = (l + r) // 2
+                if qr <= mid:
+                    return find_leftmost(idx * 2, l, mid, qr, sumRight)
+                # qr is in the right child => left child fully included, right child partially
+                right_sum_included = bit.range_sum(mid + 1, qr)
+                res = find_leftmost(idx * 2, l, mid, qr, sumRight + right_sum_included)
+                if res is not None:
+                    return res
+                return find_leftmost(idx * 2 + 1, mid + 1, r, qr, sumRight)
 
         ans = 0
-        last_pos = {}
-        
-        for r in range(n):
-            x = nums[r]
-            # De-activate D[r] by setting it to 0 (it was inf)
-            update(1, 0, size - 1, r, r, -inf)
-            
-            prev = last_pos.get(x, -1)
-            val = 1 if x % 2 == 0 else -1
-            update(1, 0, size - 1, prev + 1, r, val)
-            
-            l = find_leftmost(1, 0, size - 1)
-            if l != -1 and l <= r:
-                ans = max(ans, r - l + 1)
-            
-            last_pos[x] = r
-            
+        for i, x in enumerate(nums, 1):
+            wt = 1 if (x % 2 == 0) else -1
+
+            if x in last:
+                p = last[x]
+                if w_at[p] != 0:
+                    bit.add(p, -w_at[p])
+                    update(1, 1, n, p, 0)
+                    w_at[p] = 0
+
+            last[x] = i
+            # set current position to wt
+            if w_at[i] != wt:
+                bit.add(i, wt - w_at[i])
+                update(1, 1, n, i, wt)
+                w_at[i] = wt
+
+            start = find_leftmost(1, 1, n, i, 0)
+            if start is not None:
+                ans = max(ans, i - start + 1)
+
         return ans
 # @lc code=end
