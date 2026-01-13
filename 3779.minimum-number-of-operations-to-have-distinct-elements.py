25d98ccd6fe39134ccdb2fd261808d2fe4a76ea7
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 03:36:37 +0800

    Add solution: 3779.minimum-number-of-operations-to-have-distinct-elements.py

--- 3779.minimum-number-of-operations-to-have-distinct-elements.py
+++ 3779.minimum-number-of-operations-to-have-distinct-elements.py
@@ -4,28 +4,26 @@
 # [3779] Minimum Number of Operations to Have Distinct Elements
 #
 
+from typing import List
+
 # @lc code=start
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
-        seen = set()
-        last_bad_idx = -1
-        
-        # Find the largest index i such that nums[i:] contains a duplicate.
-        # This is equivalent to finding the rightmost index i that has a duplicate at some j > i.
+
+        # p = smallest index such that nums[p:] is all distinct
+        p = n
+
+        # Values are in [1, 1e5]
+        seen = [0] * 100001
+
         for i in range(n - 1, -1, -1):
-            if nums[i] in seen:
-                last_bad_idx = i
+            v = nums[i]
+            if seen[v]:
+                # First duplicate encountered from the right => boundary found
                 break
-            seen.add(nums[i])
-        
-        # If no duplicates were found in the entire array, 0 operations are needed.
-        if last_bad_idx == -1:
-            return 0
-        
-        # We must remove elements at least until the suffix starting index is greater than last_bad_idx.
-        # The suffix starts at index 3 * k after k operations.
-        # We need 3 * k >= last_bad_idx + 1.
-        # k = ceil((last_bad_idx + 1) / 3)
-        return (last_bad_idx + 3) // 3
+            seen[v] = 1
+            p = i
+
+        return (p + 2) // 3
 # @lc code=end
