6c6bf9cca1ffd429ff2726cfed0738fa48ae8078
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:40:54 +0800

    Add solution: 2289.steps-to-make-array-non-decreasing.py

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -4,34 +4,30 @@
 # [2289] Steps to Make Array Non-decreasing
 #
 
+from typing import List
+
 # @lc code=start
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        n = len(nums)
-        # dp[i] will store the step at which nums[i] is removed.
-        # If dp[i] is 0, nums[i] is never removed.
-        dp = [0] * n
-        stack = [] # Monotonic stack to store indices of elements
-        
-        for i in range(n):
-            current_max_dp = 0
-            # Pop elements that are smaller than or equal to the current element.
-            # These elements would be removed before or at the same time as 
-            # the current element if they were to the right of a larger element.
-            while stack and nums[stack[-1]] <= nums[i]:
-                current_max_dp = max(current_max_dp, dp[stack.pop()])
-            
-            # If the stack is not empty, the top element is the nearest element
-            # to the left that is strictly greater than nums[i].
-            if stack:
-                # nums[i] will be removed 1 step after all elements between 
-                # it and nums[stack[-1]] are removed.
-                dp[i] = current_max_dp + 1
+        # stack holds (value, steps_to_be_removed)
+        stack = []
+        ans = 0
+
+        for x in nums:
+            cur = 0
+            # Pop all elements <= x; they cannot be the "killer" of x.
+            # While popping, collect the max removal time among them.
+            while stack and x >= stack[-1][0]:
+                cur = max(cur, stack[-1][1])
+                stack.pop()
+
+            if not stack:
+                cur = 0
             else:
-                # No element to the left is greater than nums[i].
-                dp[i] = 0
-            
-            stack.append(i)
-            
-        return max(dp)
+                cur += 1
+
+            ans = max(ans, cur)
+            stack.append((x, cur))
+
+        return ans
 # @lc code=end
