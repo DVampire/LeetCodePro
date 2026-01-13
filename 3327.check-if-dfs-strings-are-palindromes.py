9ec15e0b6e9b23acc56391f5a93dd0b7e907fa3f
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 06:23:15 +0800

    Add solution: 3327.check-if-dfs-strings-are-palindromes.py

--- 3327.check-if-dfs-strings-are-palindromes.py
+++ 3327.check-if-dfs-strings-are-palindromes.py
@@ -4,76 +4,68 @@
 # [3327] Check if DFS Strings Are Palindromes
 #
 
-from typing import List
-
 # @lc code=start
 class Solution:
     def findAnswer(self, parent: List[int], s: str) -> List[bool]:
         n = len(parent)
+        
+        # Build adjacency list
         children = [[] for _ in range(n)]
         for i in range(1, n):
-            children[parent[i]].append(i)  # appended in increasing i => already sorted
-
+            children[parent[i]].append(i)
+        
+        # Sort children for each node
+        for i in range(n):
+            children[i].sort()
+        
+        # Build global DFS string and track positions
+        dfs_str = []
         start = [0] * n
         end = [0] * n
-        post_chars: List[str] = []
-
-        # Iterative DFS to generate postorder sequence and subtree segments.
-        # state 0 = enter, state 1 = exit
-        stack = [(0, 0)]
-        while stack:
-            u, st = stack.pop()
-            if st == 0:
-                start[u] = len(post_chars)
-                stack.append((u, 1))
-                # push children in reverse so they are processed in increasing order
-                for v in reversed(children[u]):
-                    stack.append((v, 0))
-            else:
-                post_chars.append(s[u])
-                end[u] = len(post_chars) - 1
-
-        T = ''.join(post_chars)
-        Tr = T[::-1]
-
-        mod1, mod2 = 1_000_000_007, 1_000_000_009
-        base = 911382323
-
-        pow1 = [1] * (n + 1)
-        pow2 = [1] * (n + 1)
-        for i in range(1, n + 1):
-            pow1[i] = (pow1[i - 1] * base) % mod1
-            pow2[i] = (pow2[i - 1] * base) % mod2
-
-        def build_prefix(t: str, mod: int) -> List[int]:
-            pref = [0] * (n + 1)
-            for i, ch in enumerate(t):
-                x = ord(ch) - 96  # 1..26
-                pref[i + 1] = (pref[i] * base + x) % mod
-            return pref
-
-        pref1 = build_prefix(T, mod1)
-        pref2 = build_prefix(T, mod2)
-        pref1r = build_prefix(Tr, mod1)
-        pref2r = build_prefix(Tr, mod2)
-
-        def get_hash(pref: List[int], l: int, r: int, pw: List[int], mod: int) -> int:
-            # inclusive l..r
-            return (pref[r + 1] - (pref[l] * pw[r - l + 1]) % mod) % mod
-
-        ans = [False] * n
+        
+        def dfs(x):
+            start[x] = len(dfs_str)
+            for child in children[x]:
+                dfs(child)
+            dfs_str.append(s[x])
+            end[x] = len(dfs_str)
+        
+        dfs(0)
+        
+        # Convert to string
+        dfs_string = ''.join(dfs_str)
+        m = len(dfs_string)
+        
+        # Polynomial rolling hash
+        MOD = 10**9 + 7
+        BASE = 31
+        
+        # Forward hash
+        fwd_hash = [0] * (m + 1)
+        pow_base = [1] * (m + 1)
+        for i in range(m):
+            fwd_hash[i + 1] = (fwd_hash[i] * BASE + ord(dfs_string[i])) % MOD
+            pow_base[i + 1] = (pow_base[i] * BASE) % MOD
+        
+        # Backward hash
+        bwd_hash = [0] * (m + 1)
+        for i in range(m):
+            bwd_hash[i + 1] = (bwd_hash[i] * BASE + ord(dfs_string[m - 1 - i])) % MOD
+        
+        def get_hash_range(hash_arr, l, r):
+            return (hash_arr[r] - hash_arr[l] * pow_base[r - l]) % MOD
+        
+        answer = []
         for i in range(n):
             l, r = start[i], end[i]
-            rl, rr = n - 1 - r, n - 1 - l
-
-            h1 = get_hash(pref1, l, r, pow1, mod1)
-            h1r = get_hash(pref1r, rl, rr, pow1, mod1)
-            if h1 != h1r:
-                continue
-
-            h2 = get_hash(pref2, l, r, pow2, mod2)
-            h2r = get_hash(pref2r, rl, rr, pow2, mod2)
-            ans[i] = (h2 == h2r)
-
-        return ans
+            
+            # Forward hash
+            fwd = get_hash_range(fwd_hash, l, r)
+            
+            # Backward hash
+            bwd = get_hash_range(bwd_hash, m - r, m - l)
+            
+            answer.append(fwd == bwd)
+        
+        return answer
 # @lc code=end
