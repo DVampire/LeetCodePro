a3152fbe2beb8bc0959fe3de5ccd38d43d1b0570
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:40:11 +0800

    Add solution: 2074.reverse-nodes-in-even-length-groups.py

--- 2074.reverse-nodes-in-even-length-groups.py
+++ 2074.reverse-nodes-in-even-length-groups.py
@@ -5,48 +5,54 @@
 #
 
 # @lc code=start
+from typing import Optional
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev_group_end = head
-        group_len = 2
-        
-        while prev_group_end.next:
-            # Determine actual length of the current group
-            curr = prev_group_end.next
-            actual_len = 0
-            temp = curr
-            while temp and actual_len < group_len:
-                temp = temp.next
-                actual_len += 1
-            
-            if actual_len % 2 == 0:
-                # Reverse the current group
-                reverse_prev = None
-                reverse_curr = curr
-                for _ in range(actual_len):
-                    next_node = reverse_curr.next
-                    reverse_curr.next = reverse_prev
-                    reverse_prev = reverse_curr
-                    reverse_curr = next_node
-                
-                # Reconnect the reversed segment
-                # curr is now the end of the reversed group
-                # reverse_prev is the new head of the group
-                # reverse_curr is the start of the next group
-                prev_group_end.next = reverse_prev
-                curr.next = reverse_curr
-                prev_group_end = curr
+    def reverseEvenLengthGroups(self, head: Optional['ListNode']) -> Optional['ListNode']:
+        def reverse_k(start: 'ListNode', k: int):
+            prev = None
+            cur = start
+            for _ in range(k):
+                nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = nxt
+            # prev is new head, start is new tail
+            return prev, start
+
+        dummy = ListNode(0, head)
+        prev = dummy
+        curr = head
+        group_size = 1
+
+        while curr:
+            # Find actual length of this group and the next group's start
+            cnt = 0
+            node = curr
+            while node and cnt < group_size:
+                node = node.next
+                cnt += 1
+            next_start = node
+
+            if cnt % 2 == 0:
+                new_head, new_tail = reverse_k(curr, cnt)
+                prev.next = new_head
+                new_tail.next = next_start
+                prev = new_tail
+                curr = next_start
             else:
-                # Skip the current group
-                for _ in range(actual_len):
-                    prev_group_end = prev_group_end.next
-            
-            group_len += 1
-            
-        return head
+                # No reversal; just advance prev and curr by cnt nodes
+                for _ in range(cnt):
+                    prev = curr
+                    curr = curr.next
+
+            group_size += 1
+
+        return dummy.next
 # @lc code=end
