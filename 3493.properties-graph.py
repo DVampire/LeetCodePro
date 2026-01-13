7a94d0c26f1beb0074045e87b0e53337da7fd2b1
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 01:12:54 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -4,34 +4,42 @@
 # [3493] Properties Graph
 #
 
+from typing import List
+
 # @lc code=start
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x: int) -> int:
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, a: int, b: int) -> bool:
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return False
+        if self.size[ra] < self.size[rb]:
+            ra, rb = rb, ra
+        self.parent[rb] = ra
+        self.size[ra] += self.size[rb]
+        return True
+
+
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
-        # Convert each property list into a set to handle distinct integers efficiently
-        node_sets = [set(p) for p in properties]
-        # Initialize Disjoint Set Union (DSU) parent pointers
-        parent = list(range(n))
-        
-        def find(x):
-            # Standard find with path compression
-            if parent[x] == x:
-                return x
-            parent[x] = find(parent[x])
-            return parent[x]
-        
-        num_components = n
-        # Iterate through all unique pairs of nodes
+        sets = [set(row) for row in properties]
+
+        dsu = DSU(n)
         for i in range(n):
             for j in range(i + 1, n):
-                # Calculate the number of distinct common integers using set intersection
-                if len(node_sets[i] & node_sets[j]) >= k:
-                    root_i = find(i)
-                    root_j = find(j)
-                    # If nodes are in different components, merge them
-                    if root_i != root_j:
-                        parent[root_i] = root_j
-                        num_components -= 1
-                        
-        return num_components
+                if len(sets[i] & sets[j]) >= k:
+                    dsu.union(i, j)
+
+        roots = {dsu.find(i) for i in range(n)}
+        return len(roots)
 # @lc code=end
