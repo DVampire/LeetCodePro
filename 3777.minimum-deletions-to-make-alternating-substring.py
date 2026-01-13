b16708df41fd6d87dfd76a4d20b709df31d4a3e3
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 03:35:09 +0800

    Add solution: 3777.minimum-deletions-to-make-alternating-substring.py

--- 3777.minimum-deletions-to-make-alternating-substring.py
+++ 3777.minimum-deletions-to-make-alternating-substring.py
@@ -4,52 +4,73 @@
 # [3777] Minimum Deletions to Make Alternating Substring
 #
 
+from typing import List
+
 # @lc code=start
+class Fenwick:
+    def __init__(self, n: int):
+        self.n = n
+        self.bit = [0] * (n + 1)
+
+    def add(self, i: int, delta: int) -> None:
+        # i is 1-based
+        while i <= self.n:
+            self.bit[i] += delta
+            i += i & -i
+
+    def sum(self, i: int) -> int:
+        # prefix sum up to i (1-based), sum(0) = 0
+        res = 0
+        while i > 0:
+            res += self.bit[i]
+            i -= i & -i
+        return res
+
+    def range_sum(self, l: int, r: int) -> int:
+        # sum over [l, r], 1-based
+        if l > r:
+            return 0
+        return self.sum(r) - self.sum(l - 1)
+
+
 class Solution:
     def minDeletions(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
-        s_list = list(s)
-        bit = [0] * (n + 1)
-        
-        def update(i, delta):
-            while i < n:
-                bit[i] += delta
-                i += i & (-i)
-        
-        def query(i):
-            res = 0
-            while i > 0:
-                res += bit[i]
-                i -= i & (-i)
-            return res
+        s = list(s)
 
-        # x[i] is 1 if s[i] == s[i+1], else 0
-        x = [0] * (n - 1)
-        for i in range(n - 1):
-            if s_list[i] == s_list[i+1]:
-                x[i] = 1
-                update(i + 1, 1)
-        
-        results = []
+        # e[i] for i in [1..n-1]: 1 if s[i]==s[i-1]
+        e = [0] * n
+        fw = Fenwick(n)  # we will only use indices 1..n-1
+        for i in range(1, n):
+            e[i] = 1 if s[i] == s[i - 1] else 0
+            if e[i]:
+                fw.add(i, e[i])
+
+        ans = []
+
+        def recompute(idx: int) -> None:
+            # idx in [1..n-1]
+            nonlocal e
+            new_val = 1 if s[idx] == s[idx - 1] else 0
+            if new_val != e[idx]:
+                fw.add(idx, new_val - e[idx])
+                e[idx] = new_val
+
         for q in queries:
             if q[0] == 1:
                 j = q[1]
-                s_list[j] = 'B' if s_list[j] == 'A' else 'A'
-                # Flipping s[j] affects x[j-1] and x[j]
-                for i in (j - 1, j):
-                    if 0 <= i < n - 1:
-                        new_val = 1 if s_list[i] == s_list[i+1] else 0
-                        if new_val != x[i]:
-                            update(i + 1, new_val - x[i])
-                            x[i] = new_val
+                s[j] = 'A' if s[j] == 'B' else 'B'
+                if 1 <= j <= n - 1:
+                    recompute(j)
+                if 1 <= j + 1 <= n - 1:
+                    recompute(j + 1)
             else:
                 l, r = q[1], q[2]
-                # Minimum deletions for substring s[l..r] is sum of x[i] for i in [l, r-1]
-                # This corresponds to range [l+1, r] in 1-indexed BIT
-                if l < r:
-                    results.append(query(r) - query(l))
+                if l == r:
+                    ans.append(0)
                 else:
-                    results.append(0)
-        
-        return results
+                    # sum of e in indices [l+1..r]
+                    ans.append(fw.range_sum(l + 1, r))
+
+        return ans
 # @lc code=end
