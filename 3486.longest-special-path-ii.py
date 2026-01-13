fe8a2b7bc664f5d6aa8239255555d57568ee7058
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 01:10:57 +0800

    Add solution: 3486.longest-special-path-ii.py

--- 3486.longest-special-path-ii.py
+++ 3486.longest-special-path-ii.py
@@ -4,93 +4,100 @@
 # [3486] Longest Special Path II
 #
 
+from typing import List
+
 # @lc code=start
-import sys
-from collections import defaultdict
-
-# Increase recursion depth for deep trees
-sys.setrecursionlimit(200000)
-
 class Solution:
-    def longestSpecialPath(self, edges: list[list[int]], nums: list[int]) -> list[int]:
+    def longestSpecialPath(self, edges: List[List[int]], nums: List[int]) -> List[int]:
         n = len(nums)
         adj = [[] for _ in range(n)]
         for u, v, w in edges:
             adj[u].append((v, w))
             adj[v].append((u, w))
-            
-        # Fenwick tree to track indices in S
-        bit = [0] * (n + 1)
-        def bit_update(idx, val):
-            idx += 1 # 1-indexed
-            while idx <= n:
-                bit[idx] += val
-                idx += idx & (-idx)
-                
-        def bit_query_total():
-            idx = n
-            s = 0
-            while idx > 0:
-                s += bit[idx]
-                idx -= idx & (-idx)
-            return s
-            
-        def bit_find_kth(k):
-            idx = 0
-            current_sum = 0
-            for i in range(16, -1, -1):
-                next_idx = idx + (1 << i)
-                if next_idx <= n and current_sum + bit[next_idx] < k:
-                    idx = next_idx
-                    current_sum += bit[idx]
-            return idx # Returns 0-indexed position (since we used 1-indexed in bit)
 
-        max_len = -1
-        min_nodes = float('inf')
-        
-        val_indices = defaultdict(list)
-        path_dists = [0] # Prefix sums of weights
-        
-        def dfs(u, p, current_dist, depth):
-            nonlocal max_len, min_nodes
-            
-            val = nums[u]
-            added_idx = -1
-            if val_indices[val]:
-                added_idx = val_indices[val][-1]
-                bit_update(added_idx, 1)
-            
-            val_indices[val].append(depth)
-            path_dists.append(current_dist)
-            
-            total_s = bit_query_total()
-            if total_s < 2:
-                min_i = 0
+        maxv = max(nums) if nums else 0
+        freq = [0] * (maxv + 1)
+        twos = 0
+        threes = 0
+
+        path_vals: List[int] = []
+        path_dist: List[int] = []
+
+        ops: List[tuple[int, int]] = []  # (val, delta)
+        l = 0
+
+        def apply(val: int, delta: int) -> None:
+            nonlocal twos, threes
+            prev = freq[val]
+            new = prev + delta
+
+            if prev == 2:
+                twos -= 1
+            elif prev == 3:
+                threes -= 1
+
+            if new == 2:
+                twos += 1
+            elif new == 3:
+                threes += 1
+
+            freq[val] = new
+
+        def do(val: int, delta: int) -> None:
+            apply(val, delta)
+            ops.append((val, delta))
+
+        bestLen = -1
+        bestNodes = 10**18
+
+        # stack frames:
+        # enter: (u, parent, dist, 0)
+        # exit : (u, parent, dist, 1, l0, ops_len)
+        stack = [(0, -1, 0, 0)]
+
+        while stack:
+            frame = stack.pop()
+            if frame[3] == 0:
+                u, parent, dist, _ = frame
+                l0 = l
+                ops_len = len(ops)
+
+                # enter node
+                path_vals.append(nums[u])
+                path_dist.append(dist)
+                do(nums[u], +1)
+
+                # shrink until valid
+                while threes > 0 or twos > 1:
+                    rem_val = path_vals[l]
+                    do(rem_val, -1)
+                    l += 1
+
+                # evaluate best suffix [l..r]
+                curLen = path_dist[-1] - path_dist[l]
+                curNodes = len(path_vals) - l
+                if curLen > bestLen:
+                    bestLen = curLen
+                    bestNodes = curNodes
+                elif curLen == bestLen and curNodes < bestNodes:
+                    bestNodes = curNodes
+
+                # schedule exit then children
+                stack.append((u, parent, dist, 1, l0, ops_len))
+                for v, w in reversed(adj[u]):
+                    if v == parent:
+                        continue
+                    stack.append((v, u, dist + w, 0))
+
             else:
-                # Second largest is the (total_s - 1)-th smallest
-                max2 = bit_find_kth(total_s - 1)
-                min_i = max2 + 1
-            
-            curr_path_len = current_dist - path_dists[min_i]
-            curr_num_nodes = depth - min_i + 1
-            
-            if curr_path_len > max_len:
-                max_len = curr_path_len
-                min_nodes = curr_num_nodes
-            elif curr_path_len == max_len:
-                if curr_num_nodes < min_nodes:
-                    min_nodes = curr_num_nodes
-            
-            for v, w in adj[u]:
-                if v != p:
-                    dfs(v, u, current_dist + w, depth + 1)
-            
-            # Backtrack
-            path_dists.pop()
-            val_indices[val].pop()
-            if added_idx != -1:
-                bit_update(added_idx, -1)
+                # exit node: rollback to saved state
+                _, _, _, _, l0, ops_len = frame
+                while len(ops) > ops_len:
+                    val, delta = ops.pop()
+                    apply(val, -delta)
+                l = l0
+                path_vals.pop()
+                path_dist.pop()
 
-        dfs(0, -1, 0, 0)
-        return [max_len, min_nodes]
+        return [bestLen, bestNodes]
 # @lc code=end
