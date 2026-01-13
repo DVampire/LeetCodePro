f851bcb2a2650823c13417b938ec469e4b93a36c
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:43:56 +0800

    Add solution: 3217.delete-nodes-from-linked-list-present-in-array.py

--- 3217.delete-nodes-from-linked-list-present-in-array.py
+++ 3217.delete-nodes-from-linked-list-present-in-array.py
@@ -4,6 +4,8 @@
 # [3217] Delete Nodes From Linked List Present in Array
 #
 
+from typing import List, Optional
+
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -11,23 +13,17 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        # Convert nums to a set for O(1) average lookup time
-        lookup = set(nums)
-        
-        # Use a dummy node to simplify edge cases where the head is removed
-        dummy = ListNode(0)
-        dummy.next = head
-        current = dummy
-        
-        # Iterate through the list
-        while current.next:
-            if current.next.val in lookup:
-                # Skip the node if its value is in the lookup set
-                current.next = current.next.next
+    def modifiedList(self, nums: List[int], head: Optional['ListNode']) -> Optional['ListNode']:
+        to_delete = set(nums)
+
+        dummy = ListNode(0, head)
+        cur = dummy
+
+        while cur.next:
+            if cur.next.val in to_delete:
+                cur.next = cur.next.next
             else:
-                # Move to the next node if the value is not in the set
-                current = current.next
-        
+                cur = cur.next
+
         return dummy.next
 # @lc code=end
