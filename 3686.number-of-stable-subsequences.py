4f4bfc5606578714fa6a5fb10d47c275bb5c95ca
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 02:49:27 +0800

    Add solution: 3686.number-of-stable-subsequences.py

--- 3686.number-of-stable-subsequences.py
+++ 3686.number-of-stable-subsequences.py
@@ -5,36 +5,34 @@
 #
 
 # @lc code=start
+from typing import List
+
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
-        # e0: ends in 1 even, e1: ends in 2 evens
-        # o0: ends in 1 odd, o1: ends in 2 odds
-        e0, e1, o0, o1 = 0, 0, 0, 0
-        
+
+        # dp0_1, dp0_2: ending with even, run length 1 or 2
+        # dp1_1, dp1_2: ending with odd,  run length 1 or 2
+        dp0_1 = dp0_2 = dp1_1 = dp1_2 = 0
+
         for x in nums:
-            if x % 2 == 0:
-                # New e0 can be formed by adding current even to:
-                # - empty subsequence (1 way)
-                # - any subsequence ending in odd (o0 + o1)
-                new_e0 = (1 + o0 + o1) % MOD
-                # New e1 can be formed by adding current even to:
-                # - any subsequence ending in exactly 1 even (e0)
-                new_e1 = e0
-                
-                e0 = (e0 + new_e0) % MOD
-                e1 = (e1 + new_e1) % MOD
+            b = x & 1
+            if b == 0:
+                old0_1, old0_2, old1_1, old1_2 = dp0_1, dp0_2, dp1_1, dp1_2
+
+                add_to_0_1 = (1 + old1_1 + old1_2) % MOD          # start or switch from odd
+                add_to_0_2 = old0_1 % MOD                         # extend even run 1 -> 2
+
+                dp0_1 = (dp0_1 + add_to_0_1) % MOD
+                dp0_2 = (dp0_2 + add_to_0_2) % MOD
             else:
-                # New o0 can be formed by adding current odd to:
-                # - empty subsequence (1 way)
-                # - any subsequence ending in even (e0 + e1)
-                new_o0 = (1 + e0 + e1) % MOD
-                # New o1 can be formed by adding current odd to:
-                # - any subsequence ending in exactly 1 odd (o0)
-                new_o1 = o0
-                
-                o0 = (o0 + new_o0) % MOD
-                o1 = (o1 + new_o1) % MOD
-                
-        return (e0 + e1 + o0 + o1) % MOD
+                old0_1, old0_2, old1_1, old1_2 = dp0_1, dp0_2, dp1_1, dp1_2
+
+                add_to_1_1 = (1 + old0_1 + old0_2) % MOD          # start or switch from even
+                add_to_1_2 = old1_1 % MOD                         # extend odd run 1 -> 2
+
+                dp1_1 = (dp1_1 + add_to_1_1) % MOD
+                dp1_2 = (dp1_2 + add_to_1_2) % MOD
+
+        return (dp0_1 + dp0_2 + dp1_1 + dp1_2) % MOD
 # @lc code=end
