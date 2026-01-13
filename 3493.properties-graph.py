088d75dd478ec2c3d96b41e573654596d988ae1b
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 07:37:07 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -3,43 +3,37 @@
 #
 # [3493] Properties Graph
 #
-
-from typing import List
-
 # @lc code=start
-class DSU:
-    def __init__(self, n: int):
-        self.parent = list(range(n))
-        self.size = [1] * n
-
-    def find(self, x: int) -> int:
-        while self.parent[x] != x:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-        return x
-
-    def union(self, a: int, b: int) -> bool:
-        ra, rb = self.find(a), self.find(b)
-        if ra == rb:
-            return False
-        if self.size[ra] < self.size[rb]:
-            ra, rb = rb, ra
-        self.parent[rb] = ra
-        self.size[ra] += self.size[rb]
-        return True
-
-
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
-        sets = [set(row) for row in properties]
-
-        dsu = DSU(n)
+        
+        # Function to count distinct common integers
+        def intersect(a, b):
+            return len(set(a) & set(b))
+        
+        # Build adjacency list
+        graph = [[] for _ in range(n)]
         for i in range(n):
             for j in range(i + 1, n):
-                if len(sets[i] & sets[j]) >= k:
-                    dsu.union(i, j)
-
-        roots = {dsu.find(i) for i in range(n)}
-        return len(roots)
+                if intersect(properties[i], properties[j]) >= k:
+                    graph[i].append(j)
+                    graph[j].append(i)
+        
+        # Count connected components using DFS
+        visited = [False] * n
+        components = 0
+        
+        def dfs(node):
+            visited[node] = True
+            for neighbor in graph[node]:
+                if not visited[neighbor]:
+                    dfs(neighbor)
+        
+        for i in range(n):
+            if not visited[i]:
+                dfs(i)
+                components += 1
+        
+        return components
 # @lc code=end
