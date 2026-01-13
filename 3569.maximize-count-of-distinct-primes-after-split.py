17f0920de8e301cc2f4170fc37e64a53314b0039
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 02:00:30 +0800

    Add solution: 3569.maximize-count-of-distinct-primes-after-split.py

--- 3569.maximize-count-of-distinct-primes-after-split.py
+++ 3569.maximize-count-of-distinct-primes-after-split.py
@@ -5,69 +5,142 @@
 #
 
 # @lc code=start
+from typing import List
+from collections import defaultdict
+import heapq
+
+class SegTree:
+    def __init__(self, n: int):
+        self.n = n
+        self.mx = [0] * (4 * n)
+        self.lazy = [0] * (4 * n)
+
+    def _apply(self, node: int, val: int):
+        self.mx[node] += val
+        self.lazy[node] += val
+
+    def _push(self, node: int):
+        if self.lazy[node] != 0:
+            v = self.lazy[node]
+            self._apply(node * 2, v)
+            self._apply(node * 2 + 1, v)
+            self.lazy[node] = 0
+
+    def add(self, L: int, R: int, val: int):
+        if L > R or self.n <= 0:
+            return
+        self._add(1, 1, self.n, L, R, val)
+
+    def _add(self, node: int, nl: int, nr: int, L: int, R: int, val: int):
+        if L <= nl and nr <= R:
+            self._apply(node, val)
+            return
+        self._push(node)
+        mid = (nl + nr) // 2
+        if L <= mid:
+            self._add(node * 2, nl, mid, L, R, val)
+        if R > mid:
+            self._add(node * 2 + 1, mid + 1, nr, L, R, val)
+        self.mx[node] = self.mx[node * 2] if self.mx[node * 2] > self.mx[node * 2 + 1] else self.mx[node * 2 + 1]
+
+    def max_all(self) -> int:
+        return self.mx[1] if self.n > 0 else 0
+
+
 class Solution:
     def maximumCount(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        MAX_VAL = 100001
-        is_prime = [True] * MAX_VAL
-        is_prime[0] = is_prime[1] = False
-        for p in range(2, int(MAX_VAL**0.5) + 1):
-            if is_prime[p]:
-                for i in range(p * p, MAX_VAL, p):
-                    is_prime[i] = False
-        
         n = len(nums)
-        tree_size = 1
-        while tree_size < n: tree_size *= 2
-        tree = [0] * (2 * tree_size)
-        lazy = [0] * (2 * tree_size)
+        m = n - 1  # split positions k in [1..n-1]
 
-        def update(l, r, val, node, start, end):
-            if lazy[node] != 0:
-                tree[node] += lazy[node]
-                if start != end:
-                    lazy[2 * node] += lazy[node]
-                    lazy[2 * node + 1] += lazy[node]
-                lazy[node] = 0
-            if start > end or start > r or end < l: return
-            if start >= l and end <= r:
-                tree[node] += val
-                if start != end:
-                    lazy[2 * node] += val
-                    lazy[2 * node + 1] += val
-                return
-            mid = (start + end) // 2
-            update(l, r, val, 2 * node, start, mid)
-            update(l, r, val, 2 * node + 1, mid + 1, end)
-            tree[node] = max(tree[2 * node], tree[2 * node + 1])
+        maxV = 100000
+        # Sieve of Eratosthenes
+        is_prime = [True] * (maxV + 1)
+        is_prime[0] = is_prime[1] = False
+        for i in range(2, int(maxV ** 0.5) + 1):
+            if is_prime[i]:
+                step = i
+                start = i * i
+                is_prime[start:maxV + 1:step] = [False] * (((maxV - start) // step) + 1)
 
-        prime_indices = collections.defaultdict(SortedList)
+        # prime value -> (pos_set, min_heap, max_heap)
+        pos_set = defaultdict(set)
+        min_heap = defaultdict(list)
+        max_heap = defaultdict(list)
+
+        def get_minmax(p: int):
+            s = pos_set[p]
+            if not s:
+                return None, None
+            hmin = min_heap[p]
+            while hmin and hmin[0] not in s:
+                heapq.heappop(hmin)
+            hmax = max_heap[p]
+            while hmax and (-hmax[0]) not in s:
+                heapq.heappop(hmax)
+            return hmin[0], -hmax[0]
+
+        def interval_of(p: int):
+            s = pos_set[p]
+            if len(s) < 2:
+                return None
+            mn, mx = get_minmax(p)
+            # crossing split k when mn < k <= mx => k in [mn+1, mx]
+            return (mn + 1, mx)
+
+        # Build initial structures
         for i, v in enumerate(nums):
-            if is_prime[v]: prime_indices[v].add(i)
+            if v <= maxV and is_prime[v]:
+                pos_set[v].add(i)
+                heapq.heappush(min_heap[v], i)
+                heapq.heappush(max_heap[v], -i)
 
-        def update_prime_contribution(p, delta):
-            if not prime_indices[p]: return
-            first, last = prime_indices[p][0], prime_indices[p][-1]
-            if first + 1 <= n - 1: update(first + 1, n - 1, delta, 1, 1, n - 1)
-            if 1 <= last: update(1, last, delta, 1, 1, n - 1)
+        seg = SegTree(m)
+        activePrimes = 0
+        for p in list(pos_set.keys()):
+            if pos_set[p]:
+                activePrimes += 1
+                inter = interval_of(p)
+                if inter is not None:
+                    L, R = inter
+                    seg.add(L, R, 1)
 
-        for p in prime_indices: update_prime_contribution(p, 1)
+        def modify_prime(p: int, idx: int, add_pos: bool):
+            nonlocal activePrimes
 
-        results = []
+            before_nonempty = len(pos_set[p]) > 0
+            old_inter = interval_of(p)
+
+            if add_pos:
+                pos_set[p].add(idx)
+                heapq.heappush(min_heap[p], idx)
+                heapq.heappush(max_heap[p], -idx)
+            else:
+                pos_set[p].remove(idx)
+
+            after_nonempty = len(pos_set[p]) > 0
+            new_inter = interval_of(p)
+
+            if old_inter is not None:
+                seg.add(old_inter[0], old_inter[1], -1)
+            if new_inter is not None:
+                seg.add(new_inter[0], new_inter[1], 1)
+
+            if not before_nonempty and after_nonempty:
+                activePrimes += 1
+            elif before_nonempty and not after_nonempty:
+                activePrimes -= 1
+
+        ans = []
         for idx, val in queries:
-            old_val = nums[idx]
-            if old_val != val:
-                if is_prime[old_val]:
-                    update_prime_contribution(old_val, -1)
-                    prime_indices[old_val].remove(idx)
-                    update_prime_contribution(old_val, 1)
-                nums[idx] = val
-                if is_prime[val]:
-                    update_prime_contribution(val, -1)
-                    prime_indices[val].add(idx)
-                    update_prime_contribution(val, 1)
-            results.append(tree[1])
-        return results
+            old = nums[idx]
+            if old <= maxV and is_prime[old]:
+                modify_prime(old, idx, add_pos=False)
 
-from sortedcontainers import SortedList
-import collections
+            nums[idx] = val
+            if val <= maxV and is_prime[val]:
+                modify_prime(val, idx, add_pos=True)
+
+            ans.append(activePrimes + seg.max_all())
+
+        return ans
 # @lc code=end
