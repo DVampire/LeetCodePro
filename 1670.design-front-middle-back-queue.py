e5f2eb9a9b2384b9c12a97fe960250e460d743b1
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-13 23:38:17 +0800

    Add solution: 1670.design-front-middle-back-queue.py

--- 1670.design-front-middle-back-queue.py
+++ 1670.design-front-middle-back-queue.py
@@ -1,3 +1,9 @@
+#
+# @lc app=leetcode id=1670 lang=python3
+#
+# [1670] Design Front Middle Back Queue
+#
+# @lc code=start
 from collections import deque
 
 class FrontMiddleBackQueue:
@@ -6,48 +12,54 @@ class FrontMiddleBackQueue:
         self.left = deque()
         self.right = deque()
 
-    def _balance(self):
-        if len(self.left) > len(self.right):
-            self.right.appendleft(self.left.pop())
-        elif len(self.right) > len(self.left) + 1:
+    def _rebalance(self) -> None:
+        # Ensure: len(left) == len(right) or len(left) == len(right) + 1
+        while len(self.left) < len(self.right):
             self.left.append(self.right.popleft())
+        while len(self.left) > len(self.right) + 1:
+            self.right.appendleft(self.left.pop())
 
     def pushFront(self, val: int) -> None:
         self.left.appendleft(val)
-        self._balance()
+        self._rebalance()
 
     def pushMiddle(self, val: int) -> None:
-        if len(self.left) == len(self.right):
-            self.right.appendleft(val)
-        else:
-            self.left.append(val)
+        # If left has one extra, shift its last to right so insertion becomes frontmost middle
+        if len(self.left) > len(self.right):
+            self.right.appendleft(self.left.pop())
+        self.left.append(val)
+        self._rebalance()
 
     def pushBack(self, val: int) -> None:
         self.right.append(val)
-        self._balance()
+        self._rebalance()
 
     def popFront(self) -> int:
-        if not self.right:
+        if not self.left and not self.right:
             return -1
-        if not self.left:
-            res = self.right.popleft()
-        else:
+        if self.left:
             res = self.left.popleft()
-        self._balance()
+        else:
+            res = self.right.popleft()
+        self._rebalance()
         return res
 
     def popMiddle(self) -> int:
-        if not self.right:
+        if not self.left and not self.right:
             return -1
-        if len(self.left) == len(self.right):
-            res = self.left.pop()
-        else:
-            res = self.right.popleft()
+        res = self.left.pop()  # frontmost middle
+        self._rebalance()
         return res
 
     def popBack(self) -> int:
-        if not self.right:
+        if not self.left and not self.right:
             return -1
-        res = self.right.pop()
-        self._balance()
+        if self.right:
+            res = self.right.pop()
+        else:
+            res = self.left.pop()
+        self._rebalance()
         return res
+
+
+# @lc code=end
