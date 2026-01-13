b5dc81522ba96a0bd95307a0300865234ad07d6c
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 02:05:35 +0800

    Add solution: 3579.minimum-steps-to-convert-string-with-operations.py

--- 3579.minimum-steps-to-convert-string-with-operations.py
+++ 3579.minimum-steps-to-convert-string-with-operations.py
@@ -8,46 +8,51 @@
 class Solution:
     def minOperations(self, word1: str, word2: str) -> int:
         n = len(word1)
-        dp = [float('inf')] * (n + 1)
+
+        def eval_cost(l: int, r: int, reversed_seg: bool) -> int:
+            # cost using only swaps (disjoint) + replacements, given a fixed orientation
+            cnt = [[0] * 26 for _ in range(26)]
+            mismatches = 0
+            m = r - l
+            for t in range(m):
+                c2 = word2[l + t]
+                if not reversed_seg:
+                    c1 = word1[l + t]
+                else:
+                    c1 = word1[r - 1 - t]
+                if c1 != c2:
+                    mismatches += 1
+                    cnt[ord(c1) - 97][ord(c2) - 97] += 1
+
+            beneficial = 0
+            for a in range(26):
+                rowa = cnt[a]
+                for b in range(a + 1, 26):
+                    x = rowa[b]
+                    y = cnt[b][a]
+                    beneficial += x if x < y else y
+
+            return mismatches - beneficial
+
+        # Precompute segment costs
+        cost = [[0] * (n + 1) for _ in range(n)]
+        for l in range(n - 1, -1, -1):
+            for r in range(l + 1, n + 1):
+                normal = eval_cost(l, r, False)
+                rev = 1 + eval_cost(l, r, True)
+                cost[l][r] = normal if normal < rev else rev
+
+        # DP over partitions
+        INF = 10 ** 9
+        dp = [INF] * (n + 1)
         dp[0] = 0
-
-        def get_cost(s, t):
-            length = len(s)
-            
-            def calc_ops(s_curr, t_curr):
-                mismatches = 0
-                counts = {} # Stores pairs (char_in_s, char_in_t)
-                for i in range(len(s_curr)):
-                    if s_curr[i] != t_curr[i]:
-                        mismatches += 1
-                        pair = (s_curr[i], t_curr[i])
-                        counts[pair] = counts.get(pair, 0) + 1
-                
-                n2cycle = 0
-                chars = list(set([p[0] for p in counts.keys()] + [p[1] for p in counts.keys()]))
-                for i in range(len(chars)):
-                    for j in range(i + 1, len(chars)):
-                        c1, c2 = chars[i], chars[j]
-                        n2cycle += min(counts.get((c1, c2), 0), counts.get((c2, c1), 0))
-                
-                return mismatches - n2cycle
-
-            # Option 1: No reverse
-            cost1 = calc_ops(s, t)
-            # Option 2: With reverse
-            cost2 = 1 + calc_ops(s[::-1], t)
-            
-            return min(cost1, cost2)
-
-        # Precompute substring costs to optimize
-        costs = [[0] * (n + 1) for _ in range(n + 1)]
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                costs[i][j] = get_cost(word1[i:j], word2[i:j])
-
         for i in range(1, n + 1):
+            best = INF
             for j in range(i):
-                dp[i] = min(dp[i], dp[j] + costs[j][i])
-        
-        return int(dp[n])
+                v = dp[j] + cost[j][i]
+                if v < best:
+                    best = v
+            dp[i] = best
+
+        return dp[n]
 # @lc code=end
