4eb1360dc8ee63b800efec1217f7f55a30aaf3ba
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 07:51:09 +0800

    Add solution: 3513.number-of-unique-xor-triplets-i.py

--- 3513.number-of-unique-xor-triplets-i.py
+++ 3513.number-of-unique-xor-triplets-i.py
@@ -3,14 +3,17 @@
 #
 # [3513] Number of Unique XOR Triplets I
 #
-
-from typing import List
-
 # @lc code=start
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
-        if n <= 2:
-            return n
-        return 1 << n.bit_length()
+        xor_values = set()
+        
+        for i in range(n):
+            for j in range(i, n):
+                for k in range(j, n):
+                    xor_val = nums[i] ^ nums[j] ^ nums[k]
+                    xor_values.add(xor_val)
+        
+        return len(xor_values)
 # @lc code=end
