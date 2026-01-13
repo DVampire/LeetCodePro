0fe11ea23f8ade8c20c76ca14f3d4636c13d9cec
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 06:12:32 +0800

    Add solution: 2074.reverse-nodes-in-even-length-groups.py

--- 2074.reverse-nodes-in-even-length-groups.py
+++ 2074.reverse-nodes-in-even-length-groups.py
@@ -3,56 +3,49 @@
 #
 # [2074] Reverse Nodes in Even Length Groups
 #
-
 # @lc code=start
-from typing import Optional
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def reverseEvenLengthGroups(self, head: Optional['ListNode']) -> Optional['ListNode']:
-        def reverse_k(start: 'ListNode', k: int):
-            prev = None
-            cur = start
-            for _ in range(k):
-                nxt = cur.next
-                cur.next = prev
-                prev = cur
-                cur = nxt
-            # prev is new head, start is new tail
-            return prev, start
-
+    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        prev = dummy
-        curr = head
+        prev_group_end = dummy
         group_size = 1
-
-        while curr:
-            # Find actual length of this group and the next group's start
-            cnt = 0
-            node = curr
-            while node and cnt < group_size:
-                node = node.next
-                cnt += 1
-            next_start = node
-
-            if cnt % 2 == 0:
-                new_head, new_tail = reverse_k(curr, cnt)
-                prev.next = new_head
-                new_tail.next = next_start
-                prev = new_tail
-                curr = next_start
-            else:
-                # No reversal; just advance prev and curr by cnt nodes
-                for _ in range(cnt):
+        
+        while prev_group_end.next:
+            # Count actual nodes in current group
+            group_start = prev_group_end.next
+            count = 0
+            current = group_start
+            
+            # Count up to group_size nodes or until end
+            while current and count < group_size:
+                current = current.next
+                count += 1
+            
+            # If count is even, reverse this group
+            if count % 2 == 0:
+                # Reverse the group
+                prev = current  # This is the node after the group
+                curr = group_start
+                for _ in range(count):
+                    next_node = curr.next
+                    curr.next = prev
                     prev = curr
-                    curr = curr.next
-
+                    curr = next_node
+                
+                # Connect the reversed group
+                prev_group_end.next = prev
+                prev_group_end = group_start
+            else:
+                # Move prev_group_end to the end of this group
+                for _ in range(count):
+                    prev_group_end = prev_group_end.next
+            
             group_size += 1
-
+        
         return dummy.next
 # @lc code=end
