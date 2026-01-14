35e0b0ea412c363001b002d5f410b16ce0b9b966
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 08:31:07 +0800

    Add solution: 3604.minimum-time-to-reach-destination-in-directed-graph.py

--- 3604.minimum-time-to-reach-destination-in-directed-graph.py
+++ 3604.minimum-time-to-reach-destination-in-directed-graph.py
@@ -3,40 +3,42 @@
 #
 # [3604] Minimum Time to Reach Destination in Directed Graph
 #
-
-from typing import List
-import heapq
-
 # @lc code=start
+import heapq
+from collections import defaultdict
+
 class Solution:
     def minTime(self, n: int, edges: List[List[int]]) -> int:
-        if n == 1:
-            return 0
-
-        g = [[] for _ in range(n)]
-        for u, v, s, e in edges:
-            g[u].append((v, s, e))
-
-        INF = 10**30
-        dist = [INF] * n
-        dist[0] = 0
+        # Build adjacency list
+        graph = defaultdict(list)
+        for u, v, start, end in edges:
+            graph[u].append((v, start, end))
+        
+        # Dijkstra's algorithm
+        visited = set()
         pq = [(0, 0)]  # (time, node)
-
+        
         while pq:
-            t, u = heapq.heappop(pq)
-            if t != dist[u]:
+            time, node = heapq.heappop(pq)
+            
+            if node in visited:
                 continue
-            if u == n - 1:
-                return t
-
-            for v, s, e in g[u]:
-                if t > e:
+            
+            visited.add(node)
+            
+            if node == n - 1:
+                return time
+            
+            for neighbor, start, end in graph[node]:
+                if neighbor in visited:
                     continue
-                depart = s if t < s else t
-                nt = depart + 1
-                if nt < dist[v]:
-                    dist[v] = nt
-                    heapq.heappush(pq, (nt, v))
-
+                
+                if time <= end:
+                    if time >= start:
+                        arrival_time = time + 1
+                    else:
+                        arrival_time = start + 1
+                    heapq.heappush(pq, (arrival_time, neighbor))
+        
         return -1
 # @lc code=end
