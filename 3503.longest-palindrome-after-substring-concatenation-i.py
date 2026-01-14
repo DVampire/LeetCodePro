173f50196176d4c7d816d28150209623dbe28f47
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 17:02:42 +0800

    Add solution: 3503.longest-palindrome-after-substring-concatenation-i.py

--- 3503.longest-palindrome-after-substring-concatenation-i.py
+++ 3503.longest-palindrome-after-substring-concatenation-i.py
@@ -3,7 +3,6 @@
 #
 # [3503] Longest Palindrome After Substring Concatenation I
 #
-
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str, t: str) -> int:
@@ -13,17 +12,13 @@ class Solution:
         max_len = 0
         n, m = len(s), len(t)
         
-        # Generate all substrings of s (including empty)
         for i in range(n + 1):
             for j in range(i, n + 1):
-                sub_s = s[i:j]
-                # Generate all substrings of t (including empty)
                 for k in range(m + 1):
                     for l in range(k, m + 1):
-                        sub_t = t[k:l]
-                        concatenated = sub_s + sub_t
-                        if is_palindrome(concatenated):
-                            max_len = max(max_len, len(concatenated))
+                        combined = s[i:j] + t[k:l]
+                        if is_palindrome(combined) and len(combined) > max_len:
+                            max_len = len(combined)
         
         return max_len
 # @lc code=end
