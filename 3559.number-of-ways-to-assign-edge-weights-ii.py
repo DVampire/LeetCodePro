1d70742bab912619763bc1db8b7037b78ba7da44
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 08:11:40 +0800

    Add solution: 3559.number-of-ways-to-assign-edge-weights-ii.py

--- 3559.number-of-ways-to-assign-edge-weights-ii.py
+++ 3559.number-of-ways-to-assign-edge-weights-ii.py
@@ -3,76 +3,47 @@
 #
 # [3559] Number of Ways to Assign Edge Weights II
 #
-
-from typing import List
-
 # @lc code=start
 class Solution:
     def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
+        from collections import defaultdict, deque
+        
         MOD = 10**9 + 7
-        n = len(edges) + 1
-
-        g = [[] for _ in range(n + 1)]
+        
+        # Build the tree
+        graph = defaultdict(list)
         for u, v in edges:
-            g[u].append(v)
-            g[v].append(u)
-
-        LOG = (n).bit_length()
-        parent = [[0] * (n + 1) for _ in range(LOG)]
-        depth = [0] * (n + 1)
-
-        # Iterative DFS from root = 1
-        stack = [(1, 0)]
-        order = [1]
-        parent[0][1] = 0
-        depth[1] = 0
-        while stack:
-            node, par = stack.pop()
-            parent[0][node] = par
-            for nei in g[node]:
-                if nei == par:
-                    continue
-                depth[nei] = depth[node] + 1
-                stack.append((nei, node))
-
-        # Build binary lifting table
-        for j in range(1, LOG):
-            pj = parent[j - 1]
-            cur = parent[j]
-            for v in range(1, n + 1):
-                cur[v] = pj[pj[v]]
-
-        def lca(a: int, b: int) -> int:
-            if depth[a] < depth[b]:
-                a, b = b, a
-            # lift a
-            diff = depth[a] - depth[b]
-            bit = 0
-            while diff:
-                if diff & 1:
-                    a = parent[bit][a]
-                diff >>= 1
-                bit += 1
-            if a == b:
-                return a
-            for j in range(LOG - 1, -1, -1):
-                if parent[j][a] != parent[j][b]:
-                    a = parent[j][a]
-                    b = parent[j][b]
-            return parent[0][a]
-
-        # Precompute powers of 2
-        pow2 = [1] * (n + 1)
-        for i in range(1, n + 1):
-            pow2[i] = (pow2[i - 1] * 2) % MOD
-
-        ans = []
-        for u, v in queries:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        # Function to find path length from u to v using BFS
+        def find_path_length(u, v):
             if u == v:
-                ans.append(0)
-                continue
-            w = lca(u, v)
-            dist = depth[u] + depth[v] - 2 * depth[w]
-            ans.append(pow2[dist - 1])  # dist > 0 here
-        return ans
+                return 0
+            
+            visited = set([u])
+            queue = deque([(u, 0)])
+            
+            while queue:
+                node, dist = queue.popleft()
+                
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        if neighbor == v:
+                            return dist + 1
+                        visited.add(neighbor)
+                        queue.append((neighbor, dist + 1))
+            
+            return 0
+        
+        result = []
+        for u, v in queries:
+            k = find_path_length(u, v)
+            if k == 0:
+                result.append(0)
+            else:
+                # Number of ways to have odd sum = 2^(k-1)
+                result.append(pow(2, k - 1, MOD))
+        
+        return result
 # @lc code=end
