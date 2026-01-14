ae0f09c2405c3b4c357ae5ad4dc0d2ae0ec378ac
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 09:00:51 +0800

    Add solution: 3695.maximize-alternating-sum-using-swaps.py

--- 3695.maximize-alternating-sum-using-swaps.py
+++ 3695.maximize-alternating-sum-using-swaps.py
@@ -3,69 +3,50 @@
 #
 # [3695] Maximize Alternating Sum Using Swaps
 #
-
-from typing import List
-from collections import defaultdict
-
 # @lc code=start
-class DSU:
-    def __init__(self, n: int):
-        self.parent = list(range(n))
-        self.size = [1] * n
-
-    def find(self, x: int) -> int:
-        while self.parent[x] != x:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-        return x
-
-    def union(self, a: int, b: int) -> None:
-        ra, rb = self.find(a), self.find(b)
-        if ra == rb:
-            return
-        if self.size[ra] < self.size[rb]:
-            ra, rb = rb, ra
-        self.parent[rb] = ra
-        self.size[ra] += self.size[rb]
-
-
 class Solution:
     def maxAlternatingSum(self, nums: List[int], swaps: List[List[int]]) -> int:
         n = len(nums)
-        dsu = DSU(n)
-
+        parent = list(range(n))
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px != py:
+                parent[px] = py
+        
+        # Build connected components
         for p, q in swaps:
-            dsu.union(p, q)
-
-        # root -> [values_list, even_count, odd_count]
-        comp_vals = defaultdict(list)
-        even_cnt = defaultdict(int)
-        odd_cnt = defaultdict(int)
-
-        for i, v in enumerate(nums):
-            r = dsu.find(i)
-            comp_vals[r].append(v)
-            if i % 2 == 0:
-                even_cnt[r] += 1
-            else:
-                odd_cnt[r] += 1
-
-        ans = 0
-        for r, vals in comp_vals.items():
-            e = even_cnt[r]
-            o = odd_cnt[r]
-            vals.sort()  # ascending
-            k = len(vals)
-
-            # prefix sums for O(1) range sums
-            pref = [0] * (k + 1)
-            for i, x in enumerate(vals):
-                pref[i + 1] = pref[i] + x
-
-            sum_small_odd = pref[o]                  # sum of smallest o values
-            sum_large_even = pref[k] - pref[k - e]   # sum of largest e values
-
-            ans += sum_large_even - sum_small_odd
-
-        return ans
+            union(p, q)
+        
+        # Group indices by their root
+        from collections import defaultdict
+        components = defaultdict(list)
+        for i in range(n):
+            components[find(i)].append(i)
+        
+        # Calculate the maximum alternating sum
+        result = 0
+        for indices in components.values():
+            # Get values at these indices
+            values = [nums[i] for i in indices]
+            
+            # Count even indices in this component
+            even_count = sum(1 for i in indices if i % 2 == 0)
+            
+            # Sort values in descending order
+            values.sort(reverse=True)
+            
+            # Assign largest values to even indices (positive contribution)
+            # and smallest values to odd indices (negative contribution)
+            for i in range(even_count):
+                result += values[i]
+            for i in range(even_count, len(values)):
+                result -= values[i]
+        
+        return result
 # @lc code=end
