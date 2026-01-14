a671805c4ec41d8a2b8e59ba9284fb2096feee1c
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 13:44:49 +0800

    Add solution: 3425.longest-special-path.py

--- 3425.longest-special-path.py
+++ 3425.longest-special-path.py
@@ -5,58 +5,67 @@
 #
 
 # @lc code=start
+from typing import List
+import sys
+
 class Solution:
     def longestSpecialPath(self, edges: List[List[int]], nums: List[int]) -> List[int]:
+        sys.setrecursionlimit(60000)
         n = len(nums)
         
         # Build adjacency list
-        graph = [[] for _ in range(n)]
+        adj = [[] for _ in range(n)]
         for u, v, length in edges:
-            graph[u].append((v, length))
-            graph[v].append((u, length))
-        
-        # Build tree structure (parent -> children)
-        children = [[] for _ in range(n)]
-        visited = [False] * n
-        
-        def build_tree(node):
-            visited[node] = True
-            for neighbor, length in graph[node]:
-                if not visited[neighbor]:
-                    children[node].append((neighbor, length))
-                    build_tree(neighbor)
-        
-        build_tree(0)
+            adj[u].append((v, length))
+            adj[v].append((u, length))
         
         max_length = 0
         min_nodes = 1
         
-        def dfs(node, current_length, node_count, seen_values):
+        # path_dist[i] = cumulative distance from root to i-th node in current path
+        path_dist = [0]
+        # last_occurrence[val] = index in path where value was last seen
+        last_occurrence = {}
+        
+        def dfs(node, parent, left):
             nonlocal max_length, min_nodes
             
-            # Update result for current path
-            if current_length > max_length:
-                max_length = current_length
-                min_nodes = node_count
-            elif current_length == max_length:
-                min_nodes = min(min_nodes, node_count)
+            val = nums[node]
+            cur_idx = len(path_dist) - 1
             
-            # Try to extend path to children
-            for child, edge_length in children[node]:
-                if nums[child] not in seen_values:
-                    seen_values.add(nums[child])
-                    dfs(child, current_length + edge_length, node_count + 1, seen_values)
-                    seen_values.remove(nums[child])
+            # Save old occurrence for backtracking
+            old_occurrence = last_occurrence.get(val, -1)
+            
+            # Update left pointer if duplicate found
+            if val in last_occurrence:
+                left = max(left, last_occurrence[val] + 1)
+            
+            # Update last occurrence
+            last_occurrence[val] = cur_idx
+            
+            # Calculate path length and nodes for path ending at current node
+            path_length = path_dist[cur_idx] - path_dist[left]
+            num_nodes = cur_idx - left + 1
+            
+            # Update answer
+            if path_length > max_length or (path_length == max_length and num_nodes < min_nodes):
+                max_length = path_length
+                min_nodes = num_nodes
+            
+            # DFS to children
+            for neighbor, weight in adj[node]:
+                if neighbor != parent:
+                    path_dist.append(path_dist[-1] + weight)
+                    dfs(neighbor, node, left)
+                    path_dist.pop()
+            
+            # Backtrack: restore last_occurrence
+            if old_occurrence == -1:
+                del last_occurrence[val]
+            else:
+                last_occurrence[val] = old_occurrence
         
-        # Explore all possible starting points
-        def explore_all(node):
-            # Start a new path from this node
-            dfs(node, 0, 1, {nums[node]})
-            # Recursively explore children
-            for child, _ in children[node]:
-                explore_all(child)
-        
-        explore_all(0)
+        dfs(0, -1, 0)
         
         return [max_length, min_nodes]
 # @lc code=end
