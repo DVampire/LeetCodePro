46c1f76f3c29fef8bc37cb262e1aa3885d36d1c4
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 14:26:49 +0800

    Add solution: 3458.select-k-disjoint-special-substrings.py

--- 3458.select-k-disjoint-special-substrings.py
+++ 3458.select-k-disjoint-special-substrings.py
@@ -3,6 +3,7 @@
 #
 # [3458] Select K Disjoint Special Substrings
 #
+
 # @lc code=start
 class Solution:
     def maxSubstringLength(self, s: str, k: int) -> bool:
@@ -11,7 +12,7 @@ class Solution:
         if k == 0:
             return True
         
-        # Precompute first and last occurrence of each character
+        # Find first and last occurrence of each character
         first = {}
         last = {}
         for i, c in enumerate(s):
@@ -19,41 +20,41 @@ class Solution:
                 first[c] = i
             last[c] = i
         
-        # Find all special substrings (as intervals [i, j])
-        special_substrings = []
+        # Find all minimal special substrings
+        intervals = set()
         
-        for i in range(n):
-            max_last = i
+        for c in first:
+            start = first[c]
+            end = last[c]
             
-            for j in range(i, n):
-                c = s[j]
+            # Expand until closed
+            i = start
+            while i <= end:
+                char = s[i]
+                new_start = first[char]
+                new_end = last[char]
                 
-                # If this character appeared before position i, break
-                if first[c] < i:
-                    break
-                
-                max_last = max(max_last, last[c])
-                
-                if max_last == j:
-                    # Exclude the entire string
-                    if not (i == 0 and j == n - 1):
-                        special_substrings.append((i, j))
+                if new_start < start:
+                    start = new_start
+                    i = start
+                else:
+                    end = max(end, new_end)
+                    i += 1
+            
+            # Check constraint: not the entire string
+            if not (start == 0 and end == n - 1):
+                intervals.add((start, end))
         
-        # Find maximum number of disjoint intervals using greedy algorithm
-        if not special_substrings:
-            return False
+        # Sort by end point for greedy scheduling
+        intervals = sorted(intervals, key=lambda x: x[1])
         
-        # Sort by ending position
-        special_substrings.sort(key=lambda x: x[1])
-        
-        # Greedy selection
-        count = 1
-        last_end = special_substrings[0][1]
-        
-        for i in range(1, len(special_substrings)):
-            if special_substrings[i][0] > last_end:
+        # Greedy: select maximum non-overlapping intervals
+        count = 0
+        prev_end = -1
+        for start, end in intervals:
+            if start > prev_end:
                 count += 1
-                last_end = special_substrings[i][1]
+                prev_end = end
         
         return count >= k
 # @lc code=end
