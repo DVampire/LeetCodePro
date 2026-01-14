e7bf91ab3b633669d3f7b778d231dc576d5374ce
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 16:05:04 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -2,38 +2,36 @@
 # @lc app=leetcode id=3493 lang=python3
 #
 # [3493] Properties Graph
-#
+
 # @lc code=start
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
         
-        # Function to count distinct common integers
-        def intersect(a, b):
-            return len(set(a) & set(b))
+        # Convert each property array to a set to get distinct elements
+        prop_sets = [set(p) for p in properties]
         
-        # Build adjacency list
-        graph = [[] for _ in range(n)]
+        # Union-Find
+        parent = list(range(n))
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            px, py = find(x), find(y)
+            if px != py:
+                parent[px] = py
+        
+        # Check each pair
         for i in range(n):
             for j in range(i + 1, n):
-                if intersect(properties[i], properties[j]) >= k:
-                    graph[i].append(j)
-                    graph[j].append(i)
+                common = len(prop_sets[i] & prop_sets[j])
+                if common >= k:
+                    union(i, j)
         
-        # Count connected components using DFS
-        visited = [False] * n
-        components = 0
-        
-        def dfs(node):
-            visited[node] = True
-            for neighbor in graph[node]:
-                if not visited[neighbor]:
-                    dfs(neighbor)
-        
-        for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                components += 1
-        
+        # Count connected components
+        components = len(set(find(i) for i in range(n)))
         return components
 # @lc code=end
