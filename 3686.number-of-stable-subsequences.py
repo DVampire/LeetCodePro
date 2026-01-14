922ffe1ea7f60279dbf72f121b54409897ba00ac
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-14 08:59:34 +0800

    Add solution: 3686.number-of-stable-subsequences.py

--- 3686.number-of-stable-subsequences.py
+++ 3686.number-of-stable-subsequences.py
@@ -3,36 +3,36 @@
 #
 # [3686] Number of Stable Subsequences
 #
-
 # @lc code=start
-from typing import List
-
 class Solution:
     def countStableSubsequences(self, nums: List[int]) -> int:
         MOD = 10**9 + 7
-
-        # dp0_1, dp0_2: ending with even, run length 1 or 2
-        # dp1_1, dp1_2: ending with odd,  run length 1 or 2
-        dp0_1 = dp0_2 = dp1_1 = dp1_2 = 0
-
-        for x in nums:
-            b = x & 1
-            if b == 0:
-                old0_1, old0_2, old1_1, old1_2 = dp0_1, dp0_2, dp1_1, dp1_2
-
-                add_to_0_1 = (1 + old1_1 + old1_2) % MOD          # start or switch from odd
-                add_to_0_2 = old0_1 % MOD                         # extend even run 1 -> 2
-
-                dp0_1 = (dp0_1 + add_to_0_1) % MOD
-                dp0_2 = (dp0_2 + add_to_0_2) % MOD
-            else:
-                old0_1, old0_2, old1_1, old1_2 = dp0_1, dp0_2, dp1_1, dp1_2
-
-                add_to_1_1 = (1 + old0_1 + old0_2) % MOD          # start or switch from even
-                add_to_1_2 = old1_1 % MOD                         # extend odd run 1 -> 2
-
-                dp1_1 = (dp1_1 + add_to_1_1) % MOD
-                dp1_2 = (dp1_2 + add_to_1_2) % MOD
-
-        return (dp0_1 + dp0_2 + dp1_1 + dp1_2) % MOD
+        
+        # States: [EMPTY, E1, E2, O1, O2]
+        # EMPTY: empty subsequence
+        # E1: ends with 1 even number
+        # E2: ends with 2 consecutive even numbers
+        # O1: ends with 1 odd number
+        # O2: ends with 2 consecutive odd numbers
+        dp = [1, 0, 0, 0, 0]
+        
+        for num in nums:
+            new_dp = dp[:]  # All subsequences that skip current element
+            parity = num % 2
+            
+            if parity == 0:  # even number
+                # Can transition from EMPTY, O1, O2 to E1
+                new_dp[1] = (new_dp[1] + dp[0] + dp[3] + dp[4]) % MOD
+                # Can transition from E1 to E2
+                new_dp[2] = (new_dp[2] + dp[1]) % MOD
+            else:  # odd number
+                # Can transition from EMPTY, E1, E2 to O1
+                new_dp[3] = (new_dp[3] + dp[0] + dp[1] + dp[2]) % MOD
+                # Can transition from O1 to O2
+                new_dp[4] = (new_dp[4] + dp[3]) % MOD
+            
+            dp = new_dp
+        
+        # Sum all non-empty stable subsequences
+        return sum(dp[1:]) % MOD
 # @lc code=end
