c981ba794af4722e831747822fb0bd2a58a2c97f
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 21:51:28 +0800

    Add solution: 3367.maximize-sum-of-weights-after-edge-removals.py

--- 3367.maximize-sum-of-weights-after-edge-removals.py
+++ 3367.maximize-sum-of-weights-after-edge-removals.py
@@ -4,46 +4,57 @@
 # [3367] Maximize Sum of Weights after Edge Removals
 #
 
-# @lc code=start
-from collections import defaultdict
+from typing import List
 
+# @lc code=start
 class Solution:
     def maximizeSumOfWeights(self, edges: List[List[int]], k: int) -> int:
+        import sys
+        sys.setrecursionlimit(300000)
+        
         n = len(edges) + 1
-        graph = defaultdict(list)
-        for u, v, w in edges:
-            graph[u].append((v, w))
-            graph[v].append((u, w))
+        graph = [[] for _ in range(n)]
+        for u,v,w in edges:
+            graph[u].append((v,w))
+            graph[v].append((u,w))
         
-        dp = {}
-        stack = [(0, -1, False)]
-        
-        while stack:
-            node, parent, processed = stack.pop()
+        def dfs(u:int , parent:int):
+            total_f_children = 0
+            gains = []
             
-            if processed:
-                gains = []
-                base_sum = 0
-                
-                for neighbor, weight in graph[node]:
-                    if neighbor == parent:
-                        continue
-                    base_sum += dp[neighbor][0]
-                    gain = weight + dp[neighbor][1] - dp[neighbor][0]
-                    if gain > 0:
-                        gains.append(gain)
-                
-                gains.sort(reverse=True)
-                
-                dp0 = base_sum + sum(gains[:k])
-                dp1 = base_sum + sum(gains[:k-1])
-                
-                dp[node] = (dp0, dp1)
-            else:
-                stack.append((node, parent, True))
-                for neighbor, weight in graph[node]:
-                    if neighbor != parent:
-                        stack.append((neighbor, node, False))
+            # Process each child
+            for v,w in graph[u]:
+                if v == parent:
+                    continue
+                f_v,g_v = dfs(v,u)
+                total_f_children += f_v
+                # Gain if we keep the edge (u,v)
+                gain = w + g_v - f_v
+                gains.append(gain)
+            
+            # Sort gains descending
+            gains.sort(reverse=True)
+            
+            # Compute f[u]: maximum when u has no parent edge (can keep up to k child edges)
+            sum_f = total_f_children
+            cnt = 0
+            for gain in gains:
+                if gain <= 0 or cnt >= k:
+                    break
+                sum_f += gain
+                cnt += 1
+            
+            # Compute g[u]: maximum when u has a parent edge kept (can keep up to k-1 child edges)
+            sum_g = total_f_children
+            cnt = 0
+            for gain in gains:
+                if gain <= 0 or cnt >= k-1:
+                    break
+                sum_g += gain
+                cnt += 1
+            
+            return sum_f ,sum_g
         
-        return dp[0][0]
+        res,_ = dfs(0,-1)
+        return res
 # @lc code=end
