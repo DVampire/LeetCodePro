53f9816d0a52d51d047ed5b2e220bc7977608b17
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 16:43:21 +0800

    Add solution: 3519.count-numbers-with-non-decreasing-digits.py

--- 3519.count-numbers-with-non-decreasing-digits.py
+++ 3519.count-numbers-with-non-decreasing-digits.py
@@ -9,64 +9,79 @@ class Solution:
     def countNumbers(self, l: str, r: str, b: int) -> int:
         MOD = 10**9 + 7
         
-        def to_base(s, base):
-            if s == "0":
+        def to_base_b(num_str, base):
+            if num_str == "0":
                 return [0]
-            digits = []
-            while s != "0":
+            
+            num = list(num_str)
+            result = []
+            
+            while not (len(num) == 1 and num[0] == '0'):
                 remainder = 0
-                new_s = ""
-                for c in s:
-                    val = remainder * 10 + int(c)
-                    new_s += str(val // base)
-                    remainder = val % base
-                digits.append(remainder)
-                s = new_s.lstrip("0") or "0"
-            return digits[::-1]
+                new_num = []
+                for digit in num:
+                    current = remainder * 10 + int(digit)
+                    quotient = current // base
+                    remainder = current % base
+                    if new_num or quotient > 0:
+                        new_num.append(str(quotient))
+                result.append(remainder)
+                num = new_num if new_num else ['0']
+            
+            return result[::-1]
+        
+        def count(num_str, base):
+            if num_str == "0":
+                return 0
+            
+            digits = to_base_b(num_str, base)
+            n = len(digits)
+            
+            memo = {}
+            
+            def dp(pos, last, tight, started):
+                if pos == n:
+                    return 1 if started else 0
+                
+                key = (pos, last, tight, started)
+                if key in memo:
+                    return memo[key]
+                
+                limit = digits[pos] if tight else base - 1
+                result = 0
+                
+                for d in range(0, limit + 1):
+                    new_tight = tight and (d == limit)
+                    new_started = started or (d > 0)
+                    
+                    if not new_started:
+                        result = (result + dp(pos + 1, -1, new_tight, False)) % MOD
+                    else:
+                        if last == -1 or d >= last:
+                            result = (result + dp(pos + 1, d, new_tight, True)) % MOD
+                
+                memo[key] = result
+                return result
+            
+            return dp(0, -1, True, False)
         
         def subtract_one(s):
             if s == "0":
                 return "-1"
             digits = list(s)
             i = len(digits) - 1
-            while digits[i] == '0':
+            while i >= 0 and digits[i] == '0':
                 digits[i] = '9'
                 i -= 1
-            digits[i] = str(int(digits[i]) - 1)
-            return ''.join(digits).lstrip('0') or '0'
+            if i >= 0:
+                digits[i] = str(int(digits[i]) - 1)
+            result = ''.join(digits).lstrip('0')
+            return result if result else '0'
         
-        def count_up_to(s, base):
-            if s == "0":
-                return 0
-            digits = to_base(s, base)
-            n = len(digits)
-            memo = {}
-            
-            def dp(pos, last_digit, tight, started):
-                if pos == n:
-                    return 1 if started else 0
-                key = (pos, last_digit, tight, started)
-                if key in memo:
-                    return memo[key]
-                upper = digits[pos] if tight else base - 1
-                result = 0
-                for d in range(upper + 1):
-                    if not started:
-                        if d == 0:
-                            result += dp(pos + 1, 0, tight and d == digits[pos], False)
-                        else:
-                            result += dp(pos + 1, d, tight and d == digits[pos], True)
-                    else:
-                        if d >= last_digit:
-                            result += dp(pos + 1, d, tight and d == digits[pos], True)
-                    result %= MOD
-                memo[key] = result
-                return result
-            
-            return dp(0, 0, True, False)
-        
-        count_r = count_up_to(r, b)
         l_minus_1 = subtract_one(l)
-        count_l_minus_1 = count_up_to(l_minus_1, b)
+        
+        count_r = count(r, b)
+        count_l_minus_1 = count(l_minus_1, b) if l_minus_1 != "-1" else 0
+        
         return (count_r - count_l_minus_1 + MOD) % MOD
 # @lc code=end
