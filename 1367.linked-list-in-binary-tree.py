64d8a2801d28dd3911897f619b882046b6fde7e2
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 22:05:55 +0800

    Add solution: 1367.linked-list-in-binary-tree.py

--- 1367.linked-list-in-binary-tree.py
+++ 1367.linked-list-in-binary-tree.py
@@ -3,6 +3,7 @@
 #
 # [1367] Linked List in Binary Tree
 #
+
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
@@ -17,45 +18,23 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        # Cache for memoization
-        cache = {}
-        
-        def match(list_node: Optional[ListNode], tree_node: Optional[TreeNode]) -> bool:
-            # If linked list is exhausted -> success
-            if not list_node:
-                return True
-            # If binary tree node doesn't exist -> failure
-            if not tree_node:
-                return False
-            
-            # Check cache using object ids
-            key = (id(list_node), id(tree_node))
-            if key in cache:
-                return cache[key]
-            
-            # Values don't match -> failure
-            if list_node.val != tree_node.val:
-                cache[key] = False
-                return False
-            
-            # Try continuing down left or right subtree
-            res = match(list_node.next, tree_node.left) or match(list_node.next, tree_node.right)
-            cache[key] = res
-            return res
-        
-        # Iterative DFS traversal of binary tree
         stack = [root]
         while stack:
             node = stack.pop()
             if not node:
                 continue
-            # Check if path starts at current node
-            if match(head, node):
+            if self.match(head, node):
                 return True
-            # Push children to stack
             stack.append(node.left)
             stack.append(node.right)
-        
-        # No matching path found
         return False
+    
+    def match(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if not head:
+            return True
+        if not root:
+            return False
+        if head.val != root.val:
+            return False
+        return self.match(head.next, root.left) or self.match(head.next, root.right)
 # @lc code=end
