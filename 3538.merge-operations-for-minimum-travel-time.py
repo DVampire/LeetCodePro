6e51626e35d470500d80cbe97fc4dd5b259f518f
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 16:46:31 +0800

    Add solution: 3538.merge-operations-for-minimum-travel-time.py

--- 3538.merge-operations-for-minimum-travel-time.py
+++ 3538.merge-operations-for-minimum-travel-time.py
@@ -3,37 +3,52 @@
 #
 # [3538] Merge Operations for Minimum Travel Time
 #
+
 # @lc code=start
+from functools import cache
+from typing import List
+
 class Solution:
     def minTravelTime(self, l: int, n: int, k: int, position: List[int], time: List[int]) -> int:
-        from functools import lru_cache
+        # Precompute prefix sums of time
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + time[i]
         
-        def calculate_travel_time(pos, t):
-            total = 0
-            for i in range(len(pos) - 1):
-                distance = pos[i+1] - pos[i]
-                total += distance * t[i]
-            return total
+        def S(a, b):
+            # Sum of time[a] to time[b] (inclusive)
+            if a > b:
+                return 0
+            return prefix[b + 1] - prefix[a]
         
-        @lru_cache(maxsize=None)
-        def solve(pos_tuple, time_tuple, merges_remaining):
-            pos = list(pos_tuple)
-            t = list(time_tuple)
-            
-            if merges_remaining == 0:
-                return calculate_travel_time(pos, t)
-            
-            n_curr = len(pos)
-            min_time = float('inf')
-            
-            for i in range(1, n_curr - 1):
-                new_pos = pos[:i] + pos[i+1:]
-                new_time = t[:i] + [t[i] + t[i+1]] + t[i+2:]
-                
-                result = solve(tuple(new_pos), tuple(new_time), merges_remaining - 1)
-                min_time = min(min_time, result)
-            
-            return min_time
+        target_kept = n - k
+        INF = 10**18
         
-        return solve(tuple(position), tuple(time), k)
+        @cache
+        def dp(prev, curr, kept):
+            # prev: previous kept sign index (-1 if none)
+            # curr: current kept sign index
+            # kept: number of signs kept so far (including curr)
+            
+            if kept == target_kept:
+                # We've kept enough signs, curr must be n-1
+                return 0 if curr == n - 1 else INF
+            
+            # Remaining signs to add
+            remaining_to_add = target_kept - kept
+            # next sign must leave room for remaining_to_add - 1 more signs after it
+            max_next = n - remaining_to_add
+            
+            result = INF
+            for next_sign in range(curr + 1, max_next + 1):
+                # Cost of segment from curr to next_sign
+                # Uses accumulated time at curr = sum(time[prev+1], ..., time[curr])
+                seg_cost = (position[next_sign] - position[curr]) * S(prev + 1, curr)
+                future_cost = dp(curr, next_sign, kept + 1)
+                result = min(result, seg_cost + future_cost)
+            
+            return result
+        
+        return dp(-1, 0, 1)
+        
 # @lc code=end
