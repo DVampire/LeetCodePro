f6104c2d42db4ddbf6e2aeaf95260f854fdb6042
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 17:08:18 +0800

    Add solution: 3513.number-of-unique-xor-triplets-i.py

--- 3513.number-of-unique-xor-triplets-i.py
+++ 3513.number-of-unique-xor-triplets-i.py
@@ -8,7 +8,7 @@
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
-        if n < 3:
+        if n <= 2:
             return n
         return 1 << n.bit_length()
 # @lc code=end
