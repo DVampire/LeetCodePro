8b007af5cc93fb594343e7a360f842c5052c13d1
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 17:17:27 +0800

    Add solution: 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.py

--- 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.py
+++ 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.py
@@ -3,18 +3,36 @@
 #
 # [3729] Count Distinct Subarrays Divisible by K in Sorted Array
 #
+
 # @lc code=start
 class Solution:
     def numGoodSubarrays(self, nums: List[int], k: int) -> int:
-        distinct_subarrays = set()
         n = len(nums)
         
+        # Compute prefix sums
+        prefix = [0] * (n + 1)
         for i in range(n):
-            current_sum = 0
-            for j in range(i, n):
-                current_sum += nums[j]
-                if current_sum % k == 0:
-                    distinct_subarrays.add(tuple(nums[i:j+1]))
+            prefix[i + 1] = prefix[i] + nums[i]
         
-        return len(distinct_subarrays)
+        from collections import defaultdict
+        counter = defaultdict(int)
+        added_up_to = -1  # We've added prefix[0], ..., prefix[added_up_to] to counter
+        count = 0
+        first_occurrence = 0  # First occurrence of the current value
+        
+        for j in range(n):
+            # Update first_occurrence if nums[j] is different from nums[j-1]
+            if j > 0 and nums[j] != nums[j - 1]:
+                first_occurrence = j
+            
+            # Update counter to include prefix[0], ..., prefix[first_occurrence]
+            while added_up_to < first_occurrence:
+                added_up_to += 1
+                counter[prefix[added_up_to] % k] += 1
+            
+            # Query for prefix[j+1] mod k
+            r = prefix[j + 1] % k
+            count += counter[r]
+        
+        return count
 # @lc code=end
