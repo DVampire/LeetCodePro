a7a4a6587702f70378319cfa2678af64868c733c
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 23:37:47 +0800

    Add solution: 3677.count-binary-palindromic-numbers.py

--- 3677.count-binary-palindromic-numbers.py
+++ 3677.count-binary-palindromic-numbers.py
@@ -3,46 +3,48 @@
 #
 # [3677] Count Binary Palindromic Numbers
 #
+
 # @lc code=start
 class Solution:
     def countBinaryPalindromes(self, n: int) -> int:
+        # If n is 0, only 0 is palindrome
         if n == 0:
             return 1
         
-        def construct_palindrome(first_half, L):
-            s = bin(first_half)[2:]
-            if L % 2 == 0:
-                return int(s + s[::-1], 2)
-            else:
-                return int(s + s[-2::-1], 2)
+        # Helper function to generate binary palindromes of given length
+        def generate_palindromes(length):
+            # length is the number of bits in the binary representation (without leading zeros)
+            # For length 1: palindromes are "0" and "1" (but note: "0" is only for number 0, which we handle separately)
+            # Actually we generate all binary strings of given length that are palindromes.
+            # For odd length: half = (length + 1) // 2 bits for first half, then mirror excluding middle.
+            # For even length: half = length // 2 bits for first half, then mirror.
+            half = (length + 1) // 2
+            res = []
+            # The first bit must be 1 because binary representation has no leading zeros (except number 0).
+            # So we iterate over all numbers from 2^(half-1) to 2^half - 1.
+            start = 1 << (half - 1) if half > 0 else 0
+            end = (1 << half) - 1
+            for i in range(start, end + 1):
+                # Build palindrome binary string
+                if length % 2 == 0:
+                    # Even: mirror whole half
+                    first_half = bin(i)[2:].zfill(half)
+                    full = first_half + first_half[::-1]
+                else:
+                    # Odd: mirror half excluding middle bit
+                    first_half = bin(i)[2:].zfill(half)
+                    full = first_half + first_half[-2::-1]   # exclude middle, i.e., reverse without last char
+                res.append(int(full, 2))
+            return res
         
-        count = 1  # Count "0"
-        L = n.bit_length()
-        
-        # Count palindromes of length 1 to L-1
-        for l in range(1, L):
-            if l == 1:
-                count += 1
-            else:
-                m = (l + 1) // 2
-                count += 1 << (m - 1)
-        
-        # Count palindromes of length L that are <= n using binary search
-        m = (L + 1) // 2
-        lo, hi = 1 << (m - 1), (1 << m) - 1
-        result = lo - 1
-        
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            palindrome = construct_palindrome(mid, L)
-            if palindrome <= n:
-                result = mid
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        
-        if result >= 1 << (m - 1):
-            count += result - (1 << (m - 1)) + 1
-        
+        # Determine max length of binary representation of n
+        max_len = n.bit_length()
+        count = 1  # for number 0
+        # For each length from 1 to max_len, generate palindromes and count those <= n
+        for l in range(1, max_len + 1):
+            pals = generate_palindromes(l)
+            for p in pals:
+                if p <= n:
+                    count += 1
         return count
 # @lc code=end
