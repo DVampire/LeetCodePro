f95a5455651f7d1272989ccb392ffb408f671606
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 16:44:18 +0800

    Add solution: 3534.path-existence-queries-in-a-graph-ii.py

--- 3534.path-existence-queries-in-a-graph-ii.py
+++ 3534.path-existence-queries-in-a-graph-ii.py
@@ -7,48 +7,99 @@
 # @lc code=start
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
-        from collections import deque, defaultdict
+        from bisect import bisect_left, bisect_right
         
-        # Build adjacency list using sets to avoid duplicates
-        graph = defaultdict(set)
+        # Sort nodes by their values
+        sorted_indices = sorted(range(n), key=lambda x: nums[x])
+        sorted_values = [nums[i] for i in sorted_indices]
         
-        # Sort nodes by their values to efficiently find neighbors
-        sorted_nodes = sorted(range(n), key=lambda x: nums[x])
+        # Map from original index to position in sorted array
+        pos = [0] * n
+        for i, idx in enumerate(sorted_indices):
+            pos[idx] = i
         
-        # For each node, connect to nodes within maxDiff
+        # Compute reach_right[i] and reach_left[i]
+        reach_right = [0] * n
         for i in range(n):
-            node = sorted_nodes[i]
-            # Look forward in sorted order
-            j = i + 1
-            while j < n and nums[sorted_nodes[j]] - nums[node] <= maxDiff:
-                neighbor = sorted_nodes[j]
-                graph[node].add(neighbor)
-                graph[neighbor].add(node)
-                j += 1
+            target = sorted_values[i] + maxDiff
+            reach_right[i] = bisect_right(sorted_values, target) - 1
         
-        def bfs(start, end):
-            if start == end:
-                return 0
-            
-            visited = set([start])
-            queue = deque([(start, 0)])
-            
-            while queue:
-                curr, dist = queue.popleft()
-                
-                for neighbor in graph[curr]:
-                    if neighbor == end:
-                        return dist + 1
-                    
-                    if neighbor not in visited:
-                        visited.add(neighbor)
-                        queue.append((neighbor, dist + 1))
-            
-            return -1
+        reach_left = [0] * n
+        for i in range(n):
+            target = sorted_values[i] - maxDiff
+            reach_left[i] = bisect_left(sorted_values, target)
         
-        result = []
+        # Binary lifting for rightward jumps
+        LOG = 18
+        jump_right = [[0] * n for _ in range(LOG)]
+        cnt_right = [[0] * n for _ in range(LOG)]
+        
+        for i in range(n):
+            if reach_right[i] > i:
+                jump_right[0][i] = reach_right[i]
+                cnt_right[0][i] = 1
+            else:
+                jump_right[0][i] = i
+                cnt_right[0][i] = 0
+        
+        for k in range(1, LOG):
+            for i in range(n):
+                j = jump_right[k-1][i]
+                jump_right[k][i] = jump_right[k-1][j]
+                cnt_right[k][i] = cnt_right[k-1][i] + cnt_right[k-1][j]
+        
+        # Binary lifting for leftward jumps
+        jump_left = [[0] * n for _ in range(LOG)]
+        cnt_left = [[0] * n for _ in range(LOG)]
+        
+        for i in range(n):
+            if reach_left[i] < i:
+                jump_left[0][i] = reach_left[i]
+                cnt_left[0][i] = 1
+            else:
+                jump_left[0][i] = i
+                cnt_left[0][i] = 0
+        
+        for k in range(1, LOG):
+            for i in range(n):
+                j = jump_left[k-1][i]
+                jump_left[k][i] = jump_left[k-1][j]
+                cnt_left[k][i] = cnt_left[k-1][i] + cnt_left[k-1][j]
+        
+        # Answer queries
+        results = []
         for u, v in queries:
-            result.append(bfs(u, v))
+            pu, pv = pos[u], pos[v]
+            if pu == pv:
+                results.append(0)
+                continue
+            
+            if pu < pv:
+                # Going right
+                jumps = 0
+                curr = pu
+                for k in range(LOG - 1, -1, -1):
+                    if jump_right[k][curr] < pv:
+                        jumps += cnt_right[k][curr]
+                        curr = jump_right[k][curr]
+                
+                if reach_right[curr] >= pv:
+                    results.append(jumps + 1)
+                else:
+                    results.append(-1)
+            else:
+                # Going left (pu > pv)
+                jumps = 0
+                curr = pu
+                for k in range(LOG - 1, -1, -1):
+                    if jump_left[k][curr] > pv:
+                        jumps += cnt_left[k][curr]
+                        curr = jump_left[k][curr]
+                
+                if reach_left[curr] <= pv:
+                    results.append(jumps + 1)
+                else:
+                    results.append(-1)
         
-        return result
+        return results
 # @lc code=end
