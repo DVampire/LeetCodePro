660c165427d56430023ac5ea2148368f72c57a04
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 16:53:39 +0800

    Add solution: 3598.longest-common-prefix-between-adjacent-strings-after-removals.py

--- 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
+++ 3598.longest-common-prefix-between-adjacent-strings-after-removals.py
@@ -3,68 +3,58 @@
 #
 # [3598] Longest Common Prefix Between Adjacent Strings After Removals
 #
+
 # @lc code=start
 class Solution:
     def longestCommonPrefix(self, words: List[str]) -> List[int]:
         n = len(words)
-        
-        def common_prefix_length(s1, s2):
-            length = 0
-            for c1, c2 in zip(s1, s2):
-                if c1 == c2:
-                    length += 1
-                else:
-                    break
-            return length
-        
-        if n <= 1:
+        if n <= 2:
             return [0] * n
         
-        # Precompute lcp for all adjacent pairs
-        lcp = []
-        for i in range(n - 1):
-            lcp.append(common_prefix_length(words[i], words[i + 1]))
+        def compute_lcp(s1, s2):
+            length = min(len(s1), len(s2))
+            for k in range(length):
+                if s1[k] != s2[k]:
+                    return k
+            return length
         
-        # Precompute lcp for pairs (i-1, i+1) for all i in [1, n-2]
-        new_lcp = [0] * n
-        for i in range(1, n - 1):
-            new_lcp[i] = common_prefix_length(words[i - 1], words[i + 1])
+        # Compute LCP for all adjacent pairs
+        lcp = [compute_lcp(words[j], words[j + 1]) for j in range(n - 1)]
         
-        # Precompute prefix_max and suffix_max
-        m = len(lcp)  # m = n - 1
-        prefix_max = [0] * m
-        suffix_max = [0] * m
-        
+        # Compute prefix max: prefix_max[j] = max(lcp[0], ..., lcp[j])
+        prefix_max = [0] * (n - 1)
         prefix_max[0] = lcp[0]
-        for j in range(1, m):
+        for j in range(1, n - 1):
             prefix_max[j] = max(prefix_max[j - 1], lcp[j])
         
-        suffix_max[m - 1] = lcp[m - 1]
-        for j in range(m - 2, -1, -1):
+        # Compute suffix max: suffix_max[j] = max(lcp[j], ..., lcp[n-2])
+        suffix_max = [0] * (n - 1)
+        suffix_max[n - 2] = lcp[n - 2]
+        for j in range(n - 3, -1, -1):
             suffix_max[j] = max(suffix_max[j + 1], lcp[j])
         
-        answer = []
-        for i in range(n):
-            if i == 0:
-                # Remove first element
-                if m > 1:
-                    answer.append(suffix_max[1])
-                else:
-                    answer.append(0)
-            elif i == n - 1:
-                # Remove last element
-                if m > 1:
-                    answer.append(prefix_max[m - 2])
-                else:
-                    answer.append(0)
-            else:
-                # Remove middle element
-                candidates = [new_lcp[i]]
-                if i - 2 >= 0:
-                    candidates.append(prefix_max[i - 2])
-                if i + 1 <= m - 1:
-                    candidates.append(suffix_max[i + 1])
-                answer.append(max(candidates))
+        answer = [0] * n
+        
+        # Remove index 0: remaining LCPs are lcp[1], ..., lcp[n-2]
+        answer[0] = suffix_max[1]
+        
+        # Remove index n-1: remaining LCPs are lcp[0], ..., lcp[n-3]
+        answer[n - 1] = prefix_max[n - 3]
+        
+        # Remove index i (0 < i < n-1)
+        for i in range(1, n - 1):
+            # New pair formed: (i-1, i+1)
+            max_val = compute_lcp(words[i - 1], words[i + 1])
+            
+            # Pairs before: lcp[0], ..., lcp[i-2]
+            if i >= 2:
+                max_val = max(max_val, prefix_max[i - 2])
+            
+            # Pairs after: lcp[i+1], ..., lcp[n-2]
+            if i <= n - 3:
+                max_val = max(max_val, suffix_max[i + 1])
+            
+            answer[i] = max_val
         
         return answer
 # @lc code=end
