ccd41d5fe83aa29f966f9a4f301d3be91dfcf22a
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 17:26:08 +0800

    Add solution: 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.py
@@ -7,46 +7,54 @@
 # @lc code=start
 class Solution:
     def minInversionCount(self, nums: List[int], k: int) -> int:
-        class BIT:
-            def __init__(self, n):
-                self.n = n
-                self.tree = [0] * (n + 1)
-            
-            def update(self, i, delta=1):
-                while i <= self.n:
-                    self.tree[i] += delta
-                    i += i & (-i)
-            
-            def query(self, i):
-                if i <= 0:
-                    return 0
-                s = 0
-                while i > 0:
-                    s += self.tree[i]
-                    i -= i & (-i)
-                return s
-        
         n = len(nums)
-        min_inversions = float('inf')
+        if k == 1:
+            return 0
         
-        for i in range(n - k + 1):
-            subarray = nums[i:i+k]
-            
-            # Coordinate compression
-            sorted_vals = sorted(set(subarray))
-            val_to_idx = {v: idx + 1 for idx, v in enumerate(sorted_vals)}
-            
-            # Count inversions using BIT
-            bit = BIT(len(sorted_vals))
-            inversions = 0
-            
-            for j in range(k):
-                compressed = val_to_idx[subarray[j]]
-                # Count inversions: elements before j that are greater than subarray[j]
-                inversions += j - bit.query(compressed)
-                bit.update(compressed)
-            
-            min_inversions = min(min_inversions, inversions)
+        # Coordinate compression
+        sorted_nums = sorted(set(nums))
+        rank = {v: i + 1 for i, v in enumerate(sorted_nums)}
+        max_rank = len(sorted_nums)
         
-        return min_inversions
+        # BIT for counting
+        bit = [0] * (max_rank + 1)
+        
+        def update(i, delta=1):
+            while i <= max_rank:
+                bit[i] += delta
+                i += i & (-i)
+        
+        def query(i):
+            s = 0
+            while i > 0:
+                s += bit[i]
+                i -= i & (-i)
+            return s
+        
+        # Count inversions in first window
+        inv_count = 0
+        for i in range(k):
+            r = rank[nums[i]]
+            # Count elements > nums[i] already added
+            inv_count += query(max_rank) - query(r)
+            update(r)
+        
+        min_inv = inv_count
+        
+        # Slide the window
+        for i in range(n - k):
+            # Remove nums[i] from window
+            r_old = rank[nums[i]]
+            update(r_old, -1)
+            removed_inv = query(r_old - 1)  # Elements < nums[i] remaining in window
+            
+            # Add nums[i+k] to window
+            r_new = rank[nums[i + k]]
+            added_inv = query(max_rank) - query(r_new)  # Elements > nums[i+k] in window
+            update(r_new)
+            
+            inv_count = inv_count - removed_inv + added_inv
+            min_inv = min(min_inv, inv_count)
+        
+        return min_inv
 # @lc code=end
