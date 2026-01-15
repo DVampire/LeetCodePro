81307b2024e982eaaeecba0d4f5ce17e4447424f
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-15 22:54:41 +0800

    Add solution: 3493.properties-graph.py

--- 3493.properties-graph.py
+++ 3493.properties-graph.py
@@ -4,34 +4,38 @@
 # [3493] Properties Graph
 #
 
+from typing import List
+from collections import defaultdict, deque
+
 # @lc code=start
 class Solution:
     def numberOfComponents(self, properties: List[List[int]], k: int) -> int:
         n = len(properties)
-        
-        # Convert each property to a set of distinct elements
-        property_sets = [set(prop) for prop in properties]
-        
-        # Union-Find
-        parent = list(range(n))
-        
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-        
-        def union(x, y):
-            px, py = find(x), find(y)
-            if px != py:
-                parent[px] = py
-        
+        # Convert each row to a set of distinct integers
+        sets = [set(row) for row in properties]
+        # Build adjacency list based on intersect condition
+        adj = [[] for _ in range(n)]
         for i in range(n):
-            for j in range(i + 1, n):
-                # Calculate intersection size
-                common = len(property_sets[i] & property_sets[j])
-                if common >= k:
-                    union(i, j)
-        
-        # Count unique components
-        return len(set(find(i) for i in range(n)))
+            for j in range(i+1, n):
+                # Compute intersection size efficiently
+                inter = len(sets[i] & sets[j])
+                if inter >= k:
+                    adj[i].append(j)
+                    adj[j].append(i)
+        # Count connected components using BFS/DFS
+        visited = [False] * n
+        components = 0
+        for i in range(n):
+            if not visited[i]:
+                components += 1
+                # BFS to mark all nodes in the same component
+                q = deque([i])
+                visited[i] = True
+                while q:
+                    u = q.popleft()
+                    for v in adj[u]:
+                        if not visited[v]:
+                            visited[v] = True
+                            q.append(v)
+        return components
 # @lc code=end
