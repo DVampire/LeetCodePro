9a29a05c9d3910bbd2802920543f78d315b5041b
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-16 09:49:25 +0800

    Add solution: 3420.count-non-decreasing-subarrays-after-k-operations.py

--- 3420.count-non-decreasing-subarrays-after-k-operations.py
+++ 3420.count-non-decreasing-subarrays-after-k-operations.py
@@ -3,38 +3,28 @@
 #
 # [3420] Count Non-Decreasing Subarrays After K Operations
 #
-
 # @lc code=start
-from collections import deque
-from typing import List
-
 class Solution:
     def countNonDecreasingSubarrays(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        result = 0
-        dq = deque()  # monotonic decreasing deque of indices (by value)
-        cost = 0
+        count = 0
         
-        r = n
-        for l in range(n - 1, -1, -1):
-            # Add l to the window [l, r-1]
-            # Merge pillars with values <= nums[l]
-            while dq and nums[dq[0]] <= nums[l]:
-                old_pillar = dq.popleft()
-                next_pos = dq[0] if dq else r
-                count = next_pos - old_pillar
-                cost += (nums[l] - nums[old_pillar]) * count
-            dq.appendleft(l)
+        # For each starting position
+        for i in range(n):
+            cost = 0
+            max_so_far = nums[i]
             
-            # Shrink from the right if cost > k
-            while r > l and cost > k:
-                r -= 1
-                # Remove element at position r
-                cost -= nums[dq[-1]] - nums[r]
-                if dq[-1] == r:
-                    dq.pop()
-            
-            result += r - l
+            # Extend ending position as far as possible
+            for j in range(i, n):
+                if nums[j] < max_so_far:
+                    cost += max_so_far - nums[j]
+                else:
+                    max_so_far = nums[j]
+                
+                if cost <= k:
+                    count += 1
+                else:
+                    break
         
-        return result
+        return count
 # @lc code=end
