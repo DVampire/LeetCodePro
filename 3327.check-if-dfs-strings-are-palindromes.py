cb77f3708a8104198984d08a266eb8b9724b3bcf
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-18 22:36:48 +0800

    Add solution: 3327.check-if-dfs-strings-are-palindromes.py

--- 3327.check-if-dfs-strings-are-palindromes.py
+++ 3327.check-if-dfs-strings-are-palindromes.py
@@ -8,40 +8,65 @@
 class Solution:
     def findAnswer(self, parent: List[int], s: str) -> List[bool]:
         n = len(parent)
-        
-        # Build adjacency list for children of each node
-        children = [[] for _ in range(n)]
-        for i in range(1, n):  # Skip root node (index 0)
-            children[parent[i]].append(i)
-        
-        # Precompute subtree information
-        # For each node, we'll store the DFS string of its subtree
-        subtree_strings = [''] * n
-        
-        # First DFS to compute subtree strings
-        def dfs_subtree(node):
-            # Start with empty string for this subtree
-            subtree_str = ''
-            
-            # Process all children in order
-            for child in children[node]:
-                subtree_str += dfs_subtree(child)
-            
-            # Append current node's character
-            subtree_str += s[node]
-            
-            subtree_strings[node] = subtree_str
-            return subtree_str
-        
-        # Compute all subtree strings
-        dfs_subtree(0)
-        
-        # For each node, we need to check if its subtree string is palindrome
-        result = [False] * n
+        adj = [[] for _ in range(n)]
+        for i, p in enumerate(parent):
+            if p != -1:
+                adj[p].append(i)
         
         for i in range(n):
-            substr = subtree_strings[i]
-            result[i] = substr == substr[::-1]
+            adj[i].sort()
         
-        return result
+        # Post-order traversal to build the string T and find ranges [L, R]
+        T_list = []
+        ranges = [None] * n
+        starts = [0] * n
+        
+        # Iterative DFS to handle large trees and avoid recursion limits
+        stack = [(0, 0)] # (node, state: 0 for pre-order, 1 for post-order)
+        while stack:
+            u, state = stack.pop()
+            if state == 0:
+                starts[u] = len(T_list)
+                stack.append((u, 1))
+                # Push children in reverse order to process them in increasing order
+                for v in reversed(adj[u]):
+                    stack.append((v, 0))
+            else:
+                T_list.append(s[u])
+                ranges[u] = (starts[u], len(T_list) - 1)
+        
+        T = "".join(T_list)
+        
+        # Manacher's Algorithm to find palindromic substrings in O(n)
+        # Transform T into T_prime: "aba" -> "#a#b#a#"
+        T_prime = "#" + "#".join(T) + "#"
+        n_p = len(T_prime)
+        P = [0] * n_p
+        C = R = 0
+        for i in range(n_p):
+            if i < R:
+                P[i] = min(R - i, P[2 * C - i])
+            
+            # Attempt to expand the palindrome centered at i
+            a, b = i + (P[i] + 1), i - (P[i] + 1)
+            while a < n_p and b >= 0 and T_prime[a] == T_prime[b]:
+                P[i] += 1
+                a += 1
+                b -= 1
+            
+            # Update the center and right boundary
+            if i + P[i] > R:
+                C, R = i, i + P[i]
+        
+        # For each node, verify if its substring range in T is a palindrome
+        ans = [False] * n
+        for i in range(n):
+            L, R_idx = ranges[i]
+            length = R_idx - L + 1
+            # The center of T[L...R] in T_prime is at index L + R + 1
+            center_prime = L + R_idx + 1
+            if P[center_prime] >= length:
+                ans[i] = True
+                
+        return ans
 # @lc code=end
