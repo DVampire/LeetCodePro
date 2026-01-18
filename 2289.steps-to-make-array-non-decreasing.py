1be4afe48af8482a7a58d7c46a52bc34f1917090
Lh-Liang <Lh-Liang@users.noreply.github.com>
2026-01-18 23:29:20 +0800

    Add solution: 2289.steps-to-make-array-non-decreasing.py

--- 2289.steps-to-make-array-non-decreasing.py
+++ 2289.steps-to-make-array-non-decreasing.py
@@ -5,38 +5,35 @@
 #
 
 # @lc code=start
-from typing import List
-
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
-        # The stack will store pairs of (value, steps_to_remove)
+        n = len(nums)
+        dp = [0] * n
         stack = []
-        max_steps = 0
+        ans = 0
         
-        for x in nums:
-            current_steps = 0
-            # While the current element is greater than or equal to the top of the stack,
-            # it means the current element will 'wait' for the top element to be removed.
-            # The time it takes for the current element to be reachable by a larger element
-            # to the left is at least the time taken to remove the elements in between.
-            while stack and stack[-1][0] <= x:
-                # We take the maximum of steps needed to remove the intermediate smaller elements.
-                current_steps = max(current_steps, stack.pop()[1])
+        for i in range(n):
+            cur_max_steps = 0
+            # While the stack is not empty and the current element is greater than or equal to
+            # the element at the top of the stack, it means the current element survives longer
+            # than the elements it's 'swallowing'.
+            while stack and nums[stack[-1]] <= nums[i]:
+                # The current element nums[i] can only be removed after the element we just popped
+                # is removed. So we track the maximum steps it took for the popped elements to be removed.
+                cur_max_steps = max(cur_max_steps, dp[stack.pop()])
             
+            # If the stack is not empty, it means there is an element to the left (nums[stack[-1]])
+            # that is strictly greater than nums[i]. This element will eventually 'remove' nums[i].
             if stack:
-                # If the stack is not empty, it means there is a larger element to the left.
-                # The current element will be removed 1 step after all intermediate elements
-                # (that were smaller than it) are gone.
-                current_steps += 1
+                # nums[i] is removed one step after all the elements in between (that were swallowed)
+                # are removed.
+                dp[i] = cur_max_steps + 1
             else:
-                # If the stack is empty, no element to the left is larger than x.
-                # Therefore, x will never be removed.
-                current_steps = 0
+                # No element to the left is greater than nums[i], so it will never be removed.
+                dp[i] = 0
             
-            # Update the global maximum steps.
-            max_steps = max(max_steps, current_steps)
-            # Push the current element and its calculated removal time onto the stack.
-            stack.append((x, current_steps))
+            stack.append(i)
+            ans = max(ans, dp[i])
             
-        return max_steps
+        return ans
 # @lc code=end
