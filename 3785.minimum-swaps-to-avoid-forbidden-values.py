fbf9e0110b26c4da26343817e76d56512de91e3c
1327899890@qq.com <1327899890@qq.com@users.noreply.github.com>
2026-01-20 23:53:24 +0800

    Batch add 2 solutions: 3776.minimum-moves-t, 3785.minimum-swaps-t...

--- 3785.minimum-swaps-to-avoid-forbidden-values.py
+++ 3785.minimum-swaps-to-avoid-forbidden-values.py
@@ -3,40 +3,30 @@
 #
 # [3785] Minimum Swaps to Avoid Forbidden Values
 #
+
+from typing import List
+import collections
+
 # @lc code=start
 class Solution:
     def minSwaps(self, nums: List[int], forbidden: List[int]) -> int:
-        from collections import Counter
-        
         n = len(nums)
-        
-        num_count = Counter(nums)
-        forbidden_count = Counter(forbidden)
-        bad_count = Counter()
-        
-        total_bad = 0
+        count_nums = collections.Counter(nums)
+        count_forb = collections.Counter(forbidden)
+        for v, cnt in count_nums.items():
+            if cnt > n - count_forb[v]:
+                return -1
+        bad_count = collections.Counter()
+        k = 0
         for i in range(n):
             if nums[i] == forbidden[i]:
-                total_bad += 1
-                bad_count[nums[i]] += 1
-        
-        if total_bad == 0:
+                v = nums[i]
+                bad_count[v] += 1
+                k += 1
+        if k == 0:
             return 0
-        
-        max_count = max(bad_count.values())
-        
-        def compatible_good_count(v):
-            return n - total_bad - num_count[v] - forbidden_count[v] + 2 * bad_count[v]
-        
-        if 2 * max_count <= total_bad:  # Balanced
-            if total_bad % 2 == 1:
-                if not any(compatible_good_count(v) >= 1 for v in bad_count):
-                    return -1
-        else:  # Unbalanced
-            dominant_value = max(bad_count, key=bad_count.get)
-            unpaired = 2 * max_count - total_bad
-            if compatible_good_count(dominant_value) < unpaired:
-                return -1
-        
-        return max((total_bad + 1) // 2, max_count)
+        max_bad = max(bad_count.values())
+        m = k // 2 if max_bad <= k // 2 else k - max_bad
+        return k - m
+
 # @lc code=end
